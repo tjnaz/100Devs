81ae809c3ddb6c21f00b9848855f0d6b29e7143f
AnazThaj <anaz.thaj@gmail.com>
2022-06-30 06:48:10 +0530

    Code along class #13 temperature converter - pseudo code

--- classes/class-13-materials/temperature-converter/js/main.js
+++ classes/class-13-materials/temperature-converter/js/main.js
@@ -1,1 +1,8 @@
 //Write your pseduo code first! 
+document.querySelector('#convert').onclick = convert
+
+function convert() {
+    let celsius = document.querySelector('#celsius').value
+    let fahrenheit = (celsius * 9 / 5) + 32
+    return document.querySelector('#result').innerHTML = fahrenheit
+}
