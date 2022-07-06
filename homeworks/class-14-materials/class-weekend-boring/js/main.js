4a841d62d9acf9fc349263bdaf7309f383644780
AnazThaj <anaz.thaj@gmail.com>
2022-07-06 20:34:11 +0530

    Practice - homework redone - class-weekend-boring

--- homeworks/class-14-materials/class-weekend-boring/js/main.js
+++ homeworks/class-14-materials/class-weekend-boring/js/main.js
@@ -4,14 +4,17 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+    const inputDay = document.querySelector('#day').value
+    const day = inputDay.toLowerCase()
 
-  if(day === "tuesday" || day === "thursday"){
-    console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
-    console.log("BORING")
-  }
-
+    if (day === "tuesday" || day === "thursday") {
+        // console.log("YOU HAVE CLASS")
+        document.querySelector('#placeToSee').innerText = "YOU HAVE CLASS"
+    } else if (day === "saturday" || day === "sunday") {
+        // console.log("Its The Weekend")
+        document.querySelector('#placeToSee').innerText = "It's The Weekend"
+    } else {
+        // console.log("BORING")
+        document.querySelector('#placeToSee').innerText = "BORING"
+    }
 }
