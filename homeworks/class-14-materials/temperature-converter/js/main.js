016ef7e37c63a52ebaba8da126b0d012a2580cdb
AnazThaj <anaz.thaj@gmail.com>
2022-07-07 19:08:12 +0530

    Practice - homework redone - temperature-converter

--- homeworks/class-14-materials/temperature-converter/js/main.js
+++ homeworks/class-14-materials/temperature-converter/js/main.js
@@ -1,1 +1,6 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+const tempInCelsius = prompt("Enter the celsius value:")
+const tempInFahrehneit = (tempInCelsius * 9/5) + 32
+
+document.querySelector('#celsius').innerText = tempInCelsius
+document.querySelector('#result').innerText = tempInFahrehneit
