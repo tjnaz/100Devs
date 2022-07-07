4afa2185e2692a2ee3f3e98800b6af49a3d756d3
AnazThaj <anaz.thaj@gmail.com>
2022-07-07 14:39:27 +0530

    Practice - homework redone - simple-functions

--- homeworks/class-14-materials/simple-functions/js/main.js
+++ homeworks/class-14-materials/simple-functions/js/main.js
@@ -1,5 +1,21 @@
 //create a function that adds two numbers and alerts the sum
+function addTwoNumbers(num1, num2) {
+    return alert(num1 + num2)
+}
+
+addTwoNumbers(10, 10)
 
 //create a function that multiplys three numbers and console logs the product
+function multiplysThreeNumbers(num1, num2, num3) {
+    return console.log(num1 * num2 * num3)
+}
+
+multiplysThreeNumbers(3, 4, 5)
 
 //create a function that divides two numbers and returns the ???
+function divideTwoNumbers(num1, num2) {
+    return num1 % num2
+}
+
+const remainder = divideTwoNumbers(25, 4)
+console.log(remainder)
