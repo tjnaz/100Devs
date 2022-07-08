87e194fdfbc393ceb68f91ffafc69e67e07179e2
AnazThaj <anaz.thaj@gmail.com>
2022-07-08 07:41:04 +0530

    Practice - code along - stop-snacking

--- classes/class-16-materials/stop-snacking/js/main.js
+++ classes/class-16-materials/stop-snacking/js/main.js
@@ -1,1 +1,11 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+document.querySelector('#help').addEventListener('click', stopSnacking)
+
+function stopSnacking() {
+    let numberOfSnacks = Number(document.querySelector('input').value)
+    document.querySelector('#stops').innerText = ``
+
+    for(i = 1; i<= numberOfSnacks; i++) {
+        document.querySelector('#stops').innerText += ` STOP!`
+    }
+}
