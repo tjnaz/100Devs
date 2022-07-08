02f07c3fc1b12c706032b398d4c5ae332868d7ad
AnazThaj <anaz.thaj@gmail.com>
2022-07-08 22:05:45 +0530

    Practice - theJS-way - chapter4:loops - 1-carousal

---
+++ practice/03-the-js-way/chapter4-loops/1-carousal/main.js
@@ -0,0 +1,10 @@
+let userInput = Number(prompt("Enter number of turns you want the carousal to go:"))
+
+// for (let i = 1; i <= userInput; i++) {
+//     console.log(`Carousal turn #${i}`)
+// }
+let i = 1
+while (i <= userInput) {
+    console.log(`Carousal turn #${i}`)
+    i++
+}
