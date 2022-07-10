8a06d510b73e0ab235475b7c95b02013b07df224
AnazThaj <anaz.thaj@gmail.com>
2022-07-10 07:52:17 +0530

    Practice - theJS-way - chapter5:functions - 5-area-and-circumferance-of-a-circle

---
+++ practice/03-the-js-way/chapter5-functions/5-circumferance-and-area-of-a-circle/main.js
@@ -0,0 +1,7 @@
+const areaOfACircle = radius => Math.floor(Math.PI * (radius * radius))
+const circumferanceOfACircle = radius => Math.floor(2 * Math.PI * radius)
+
+console.log('Area of a Circle with the radius of 7')
+console.log(areaOfACircle(7))
+console.log('Circumferance of a Circle with the radius of 7')
+console.log(circumferanceOfACircle(7))
