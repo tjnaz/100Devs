eed694bb75ee23eb847eb283a55e84a5ddef6b48
AnazThaj <anaz.thaj@gmail.com>
2022-07-10 07:46:50 +0530

    Practice[update] - theJS-way - chapter5:functions - 4-calculator

--- practice/03-the-js-way/chapter5-functions/4-calculator/main.js
+++ practice/03-the-js-way/chapter5-functions/4-calculator/main.js
@@ -1,12 +1,12 @@
-const calculate = (num1, operator, num2) => {
-    (operator === '+') ? num1 + num2 :
-        (operator === '-') ? num1 - num2 :
-            (operator === '*') ? num1 * num2 :
-                (operator === '/') ? num1 / num2 :
-                    "not recognized"
-}
+const calculate = (num1, operator, num2) => (operator === '+') ? num1 + num2 :
+    (operator === '-') ? num1 - num2 :
+        (operator === '*') ? num1 * num2 :
+            (operator === '/') ? num1 / num2 :
+                "not recognized"
+
 
 console.log(calculate(4, "+", 6))
 console.log(calculate(4, "-", 6))
 console.log(calculate(2, "*", 0))
 console.log(calculate(12, "/", 0))
+console.log(calculate(12, "sad", 5))
