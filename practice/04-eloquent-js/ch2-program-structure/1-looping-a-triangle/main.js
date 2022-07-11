7d9f5a08d1b43f9b9107607b4536e8ddca525729
AnazThaj <anaz.thaj@gmail.com>
2022-07-11 06:22:48 +0530

    Practice[updated] - eloquent-js - chapter2:program-structure - 1-looping-a-triangle

--- practice/04-eloquent-js/ch2-program-structure/1-looping-a-triangle/main.js
+++ practice/04-eloquent-js/ch2-program-structure/1-looping-a-triangle/main.js
@@ -1,6 +1,13 @@
-let triangle = ''
+//  * First try solution
+// let triangle = ''
 
-for (let i = 1; i <= 7; i++) {
-    // console.log(triangle += '#')
-    document.querySelector('body').innerHTML += `<p>${triangle += '#'}`
+// for (let i = 1; i <= 7; i++) {
+//     // console.log(triangle += '#')
+//     document.querySelector('body').innerHTML += `<p>${triangle += '#'}`
+// }
+
+// * Solution after going through the solution from the book
+
+for(let i = '#'; i.length <= 7; i+='#') {
+    console.log(i)
 }
