6d25f651555ac871cba86d433f8b8a8414c4310a
AnazThaj <anaz.thaj@gmail.com>
2022-07-12 22:46:37 +0530

    Practice - eloquent-js - chapter3-functions - 0-codealong | RECURSION

--- practice/04-eloquent-js/ch3-functions/0-practice/main.js
+++ practice/04-eloquent-js/ch3-functions/0-practice/main.js
@@ -1,22 +1,33 @@
 // * ARROW FUNCTIONS => * //
 // * Optional Parameters * //
 
-const power = (base, exponent = 2) => {
-    let result = 1
-    for (count = 0; count < exponent; count++) {
-        result *= base
-    }
-    return result
-}
+// const power = (base, exponent = 2) => {
+//     let result = 1
+//     for (count = 0; count < exponent; count++) {
+//         result *= base
+//     }
+//     return result
+// }
 
-console.log(power(4))
+// console.log(power(4))
 
 // * CLOSURE * //
-function multiplier(factor) {
-    return number => number * factor
+// function multiplier(factor) {
+//     return number => number * factor
+// }
+
+// let twice = multiplier(2)
+// let thrice = multiplier(3)
+// console.log(twice(5))
+// console.log(thrice(5))
+
+// * RECURSION * //
+
+const countDown = (num) => {
+    // if (num === 0) return
+    // console.log(`first ${num}`)
+    num === 1 ? 1 : countDown(num - 1)
+    console.log(`second ${num}`)
 }
 
-let twice = multiplier(2)
-let thrice = multiplier(3)
-console.log(twice(5))
-console.log(thrice(5))
+countDown(10)
