cc148bb160fb9df072079a98312eeeb78d0da794
AnazThaj <anaz.thaj@gmail.com>
2022-07-16 21:47:01 +0530

    Practice[update] - eloquent-js - chapter3-functions - 0-codealong | GROWING FUNCTIONS

--- practice/04-eloquent-js/ch3-functions/0-practice/main.js
+++ practice/04-eloquent-js/ch3-functions/0-practice/main.js
@@ -33,3 +33,21 @@
 // countDown(10)
 
 // * GROWING FUNCTIONS * //
+
+// TODO: Need to improve this in a way that it reduces the complexity
+
+function zeroPad(number, width) {
+    let string = String(number)
+    while (string.length < width) {
+        string = "0" + string
+    }
+    return string
+}
+
+function printFarmInventory(goats, cows, camels) {
+    console.log(`${zeroPad(goats, 3)} Goats`)
+    console.log(`${zeroPad(cows, 3)} Cows`)
+    console.log(`${zeroPad(camels, 3)} Camels`)
+}
+
+printFarmInventory(5, 3, 4)
