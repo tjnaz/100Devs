fedbc4b77d8d47a286400a3ccfb1aa02c321c9b6
AnazThaj <anaz.thaj@gmail.com>
2022-07-19 22:28:48 +0530

    Practice - codecademy - lesson-1: variables | project #2-dog-years

---
+++ practice/05-codecademy/2-dog-years/main.js
@@ -0,0 +1,20 @@
+// My age
+let myAge = 29
+
+// Early years
+let earlyYears = 2
+earlyYears *= 10.5
+
+// Later years
+let laterYears = myAge - 2
+laterYears *= 4
+
+// My age in dog years
+let myAgeInDogYears = earlyYears + laterYears
+
+console.log(`Early years: ${earlyYears}. Later years: ${laterYears}`)
+
+// my name in lower case
+let myName = "Anaz".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
