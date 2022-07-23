13912f6d1a333a140b8be6a7c011496da7a0dbec
AnazThaj <anaz.thaj@gmail.com>
2022-07-23 21:26:09 +0530

    Practice first couple of examples from 'Array Methods' chapter

---
+++ practice/06-javascript-info/4-datatypes/5-array-methods/0-array-methods/main.js
@@ -0,0 +1,20 @@
+let arr = ['I', 'study', 'complex', 'language']
+
+// *SPLICE()
+// arr.splice(arr.length, 0, 'when', 'the', 'sun', 'rises')
+
+// *SLICE()
+// arr.slice(-3,-1)
+// console.log(arr.slice(-3, arr.length))
+
+// !This makes a copy of the array and doesn't affect the original array
+// let slicedArr = arr.slice()
+// slicedArr.pop()
+// console.log(slicedArr)
+
+// !This makes a copy of the array BUT, whatever happens to the copy also applies to the original array
+// let nonslicedArr = arr
+// nonslicedArr.pop()
+// console.log(nonslicedArr)
+
+console.log(arr)
