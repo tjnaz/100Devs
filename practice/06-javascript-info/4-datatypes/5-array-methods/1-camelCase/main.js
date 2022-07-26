255da8acb9aa419f97c06435edf42d35c9e0cde0
AnazThaj <anaz.thaj@gmail.com>
2022-07-26 12:23:04 +0530

    Practice the first task in the array methods - camel Casing strings

---
+++ practice/06-javascript-info/4-datatypes/5-array-methods/1-camelCase/main.js
@@ -0,0 +1,9 @@
+function camelize(str) {
+    // let splitStr = str.split('-')
+    // let capitalizeStr = splitStr.map((item,index)=> index>0? item[0].toUpperCase() + item.substring(1) : item)
+
+    // return capitalizeStr.join('')
+    return (str.split('-').map((item, index) => index > 0 ? item[0].toUpperCase() + item.substring(1) : item)).join('')
+}
+
+console.log(camelize("how-are-you-wanker"))
