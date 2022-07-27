c2ff0e04cccccee843f208fdbd70467e23d35a29
AnazThaj <anaz.thaj@gmail.com>
2022-07-27 15:03:25 +0530

    Practice the fourth task in JS Info - sort in descending order

---
+++ practice/06-javascript-info/4-datatypes/5-array-methods/4-sort-in-descending-order/main.js
@@ -0,0 +1,9 @@
+let arr = [10, 15, 245, 412, 7, 97, 16, 100, 150, 50]
+
+// *MY SOLUTION* //
+arr.sort((a, b) => a - b).reverse()
+console.log(arr)
+
+// *GIVEN SOLUTION* //
+
+// console.log(arr.sort((a, b) => b - a))
