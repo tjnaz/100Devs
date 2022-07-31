43d823b6864e3be417dc3a7543e97b7bbcd1c46d
AnazThaj <anaz.thaj@gmail.com>
2022-07-31 21:56:49 +0530

    Practice 4th chapter of Eloquent JS - Data Structures-day3 - computing correlation-day2

--- practice/04-eloquent-js/ch4-data-structures-objects-and-arrays/0-codealong/main.js
+++ practice/04-eloquent-js/ch4-data-structures-objects-and-arrays/0-codealong/main.js
@@ -1,7 +1,7 @@
 let arr = [1, 2, 3, 4, 5]
 
-console.log(arr["length"])
-console.log(arr.length)
+// console.log(arr["length"])
+// console.log(arr.length)
 
 let day1 = {
     squirrel: false,
@@ -13,22 +13,22 @@ let day1 = {
     }
 }
 
-console.log(day1.squirrel)
-console.log(day1.events[1])
-console.log(day1.wolf)
+// console.log(day1.squirrel)
+// console.log(day1.events[1])
+// console.log(day1.wolf)
 
-day1.wolf = false
-console.log(day1.wolf)
+// day1.wolf = false
+// console.log(day1.wolf)
 
-console.log(day1.descriptions)
-console.log('name' in day1.descriptions)
-console.log(Object.keys(day1))
+// console.log(day1.descriptions)
+// console.log('name' in day1.descriptions)
+// console.log(Object.keys(day1))
 
 
 // TODO Some sort of recursive stuff is going on here. Need to investigate later
 // console.log(Object.assign(day1.descriptions, day1))
 
-console.log(typeof day1.events)
+// console.log(typeof day1.events)
 
 // let journal = [
 //     {
@@ -59,17 +59,108 @@ console.log(typeof day1.events)
 // console.log(score.visitors, score.home)
 
 
-let journal = []
+let JOURNAL = [
+    { "events": ["carrot", "exercise", "weekend"], "squirrel": false },
+    { "events": ["bread", "pudding", "brushed teeth", "weekend", "touched tree"], "squirrel": false },
+    { "events": ["carrot", "nachos", "brushed teeth", "cycling", "weekend"], "squirrel": false },
+    { "events": ["brussel sprouts", "ice cream", "brushed teeth", "computer", "weekend"], "squirrel": false },
+    { "events": ["potatoes", "candy", "brushed teeth", "exercise", "weekend", "dentist"], "squirrel": false },
+    { "events": ["brussel sprouts", "pudding", "brushed teeth", "running", "weekend"], "squirrel": false },
+    { "events": ["pizza", "brushed teeth", "computer", "work", "touched tree"], "squirrel": false },
+    { "events": ["bread", "beer", "brushed teeth", "cycling", "work"], "squirrel": false },
+    { "events": ["cauliflower", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["pizza", "brushed teeth", "cycling", "work"], "squirrel": false },
+    { "events": ["lasagna", "nachos", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["brushed teeth", "weekend", "touched tree"], "squirrel": false },
+    { "events": ["lettuce", "brushed teeth", "television", "weekend"], "squirrel": false },
+    { "events": ["spaghetti", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["brushed teeth", "computer", "work"], "squirrel": false },
+    { "events": ["lettuce", "nachos", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["carrot", "brushed teeth", "running", "work"], "squirrel": false },
+    { "events": ["brushed teeth", "work"], "squirrel": false },
+    { "events": ["cauliflower", "reading", "weekend"], "squirrel": false },
+    { "events": ["bread", "brushed teeth", "weekend"], "squirrel": false },
+    { "events": ["lasagna", "brushed teeth", "exercise", "work"], "squirrel": false },
+    { "events": ["spaghetti", "brushed teeth", "reading", "work"], "squirrel": false },
+    { "events": ["carrot", "ice cream", "brushed teeth", "television", "work"], "squirrel": false },
+    { "events": ["spaghetti", "nachos", "work"], "squirrel": false },
+    { "events": ["cauliflower", "ice cream", "brushed teeth", "cycling", "work"], "squirrel": false },
+    { "events": ["spaghetti", "peanuts", "computer", "weekend"], "squirrel": true },
+    { "events": ["potatoes", "ice cream", "brushed teeth", "computer", "weekend"], "squirrel": false },
+    { "events": ["potatoes", "ice cream", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["peanuts", "brushed teeth", "running", "work"], "squirrel": false },
+    { "events": ["potatoes", "exercise", "work"], "squirrel": false },
+    { "events": ["pizza", "ice cream", "computer", "work"], "squirrel": false },
+    { "events": ["lasagna", "ice cream", "work"], "squirrel": false },
+    { "events": ["cauliflower", "candy", "reading", "weekend"], "squirrel": false },
+    { "events": ["lasagna", "nachos", "brushed teeth", "running", "weekend"], "squirrel": false },
+    { "events": ["potatoes", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["carrot", "work"], "squirrel": false },
+    { "events": ["pizza", "beer", "work", "dentist"], "squirrel": false },
+    { "events": ["lasagna", "pudding", "cycling", "work"], "squirrel": false },
+    { "events": ["spaghetti", "brushed teeth", "reading", "work"], "squirrel": false },
+    { "events": ["spaghetti", "pudding", "television", "weekend"], "squirrel": false },
+    { "events": ["bread", "brushed teeth", "exercise", "weekend"], "squirrel": false },
+    { "events": ["lasagna", "peanuts", "work"], "squirrel": true },
+    { "events": ["pizza", "work"], "squirrel": false },
+    { "events": ["potatoes", "exercise", "work"], "squirrel": false },
+    { "events": ["brushed teeth", "exercise", "work"], "squirrel": false },
+    { "events": ["spaghetti", "brushed teeth", "television", "work"], "squirrel": false },
+    { "events": ["pizza", "cycling", "weekend"], "squirrel": false },
+    { "events": ["carrot", "brushed teeth", "weekend"], "squirrel": false },
+    { "events": ["carrot", "beer", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["pizza", "peanuts", "candy", "work"], "squirrel": true },
+    { "events": ["carrot", "peanuts", "brushed teeth", "reading", "work"], "squirrel": false },
+    { "events": ["potatoes", "peanuts", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["carrot", "nachos", "brushed teeth", "exercise", "work"], "squirrel": false },
+    { "events": ["pizza", "peanuts", "brushed teeth", "television", "weekend"], "squirrel": false },
+    { "events": ["lasagna", "brushed teeth", "cycling", "weekend"], "squirrel": false },
+    { "events": ["cauliflower", "peanuts", "brushed teeth", "computer", "work", "touched tree"], "squirrel": false },
+    { "events": ["lettuce", "brushed teeth", "television", "work"], "squirrel": false },
+    { "events": ["potatoes", "brushed teeth", "computer", "work"], "squirrel": false },
+    { "events": ["bread", "candy", "work"], "squirrel": false },
+    { "events": ["potatoes", "nachos", "work"], "squirrel": false },
+    { "events": ["carrot", "pudding", "brushed teeth", "weekend"], "squirrel": false },
+    { "events": ["carrot", "brushed teeth", "exercise", "weekend", "touched tree"], "squirrel": false },
+    { "events": ["brussel sprouts", "running", "work"], "squirrel": false },
+    { "events": ["brushed teeth", "work"], "squirrel": false },
+    { "events": ["lettuce", "brushed teeth", "running", "work"], "squirrel": false },
+    { "events": ["candy", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["brussel sprouts", "brushed teeth", "computer", "work"], "squirrel": false },
+    { "events": ["bread", "brushed teeth", "weekend"], "squirrel": false },
+    { "events": ["cauliflower", "brushed teeth", "weekend"], "squirrel": false },
+    { "events": ["spaghetti", "candy", "television", "work", "touched tree"], "squirrel": false },
+    { "events": ["carrot", "pudding", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["lettuce", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["carrot", "ice cream", "brushed teeth", "cycling", "work"], "squirrel": false },
+    { "events": ["pizza", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["spaghetti", "peanuts", "exercise", "weekend"], "squirrel": true },
+    { "events": ["bread", "beer", "computer", "weekend", "touched tree"], "squirrel": false },
+    { "events": ["brushed teeth", "running", "work"], "squirrel": false },
+    { "events": ["lettuce", "peanuts", "brushed teeth", "work", "touched tree"], "squirrel": false },
+    { "events": ["lasagna", "brushed teeth", "television", "work"], "squirrel": false },
+    { "events": ["cauliflower", "brushed teeth", "running", "work"], "squirrel": false },
+    { "events": ["carrot", "brushed teeth", "running", "work"], "squirrel": false },
+    { "events": ["carrot", "reading", "weekend"], "squirrel": false },
+    { "events": ["carrot", "peanuts", "reading", "weekend"], "squirrel": true },
+    { "events": ["potatoes", "brushed teeth", "running", "work"], "squirrel": false },
+    { "events": ["lasagna", "ice cream", "work", "touched tree"], "squirrel": false },
+    { "events": ["cauliflower", "peanuts", "brushed teeth", "cycling", "work"], "squirrel": false },
+    { "events": ["pizza", "brushed teeth", "running", "work"], "squirrel": false },
+    { "events": ["lettuce", "brushed teeth", "work"], "squirrel": false },
+    { "events": ["bread", "brushed teeth", "television", "weekend"], "squirrel": false },
+    { "events": ["cauliflower", "peanuts", "brushed teeth", "weekend"], "squirrel": false }
+]
 
-function addEntry(events, squirrel) {
-    journal.push({ events, squirrel })
-}
+// function addEntry(events, squirrel) {
+//     journal.push({ events, squirrel })
+// }
 
-addEntry(["work", "touched tree", "pizza", "running", "television"], false)
-addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"], false)
-addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true)
+// addEntry(["work", "touched tree", "pizza", "running", "television"], false)
+// addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"], false)
+// addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true)
 
-console.log(journal)
+// console.log(journal)
 
 // ** COMPUTING CORRELATION ** //
 
@@ -81,4 +172,25 @@ function phi(table) {
             (table[0] + table[2]))
 }
 
-console.log(phi([76, 9, 4, 1]))
+// console.log(phi([76, 9, 4, 1]))
+
+function tableFor(event, journal) {
+    let table = [0, 0, 0, 0]
+
+    for (let i = 0; i < journal.length; i++) {
+        let entry = journal[i], index = 0
+        if (entry.events.includes(event)) {
+            index += 1
+            console.log(`entry.event.includes(event): ${index}`)
+        }
+        if (entry.squirrel) index += 2
+        console.log(`entry.squirel: ${index}`)
+        table[index] += 1
+        console.log(`table[index]: ${table[index]}`)
+    }
+    return table
+}
+
+let correlation = tableFor('pizza', JOURNAL)
+console.log(correlation)
+console.log(phi(correlation))
