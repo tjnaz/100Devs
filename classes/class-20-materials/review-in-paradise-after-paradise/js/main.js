61dd01936d3b77139db6b7824f90b91bc08415d5
AnazThaj <anaz.thaj@gmail.com>
2022-08-03 09:02:56 +0530

    Code-along class-20 - revoew-in-paradise-after-paradise

--- classes/class-20-materials/review-in-paradise-after-paradise/js/main.js
+++ classes/class-20-materials/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,12 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function compareArray(arr) {
+    if (arr[0] < arr[arr.length - 1]) {
+        return 'Hi'
+    } else if (arr[0] === arr[arr.length - 1]) {
+        return 'We close in an hour'
+    } else {
+        return 'Bye'
+    }
+}
+
+console.log(compareArray([10, 5, 2, 5, 11]))
