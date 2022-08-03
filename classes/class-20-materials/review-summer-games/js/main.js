9a690cc126182019b9a8f387e75bb4367e2150af
AnazThaj <anaz.thaj@gmail.com>
2022-08-03 09:14:48 +0530

    Code-along class-20 - summer-games

--- classes/class-20-materials/review-summer-games/js/main.js
+++ classes/class-20-materials/review-summer-games/js/main.js
@@ -1,1 +1,20 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+let sum = 1
+function multiplyArrayElements(arr) {
+    // * Simple for look
+    // for (let i = 0; i < arr.length; i++) {
+    //     sum *= arr[i]
+    // }
+
+    // * forEach method
+    arr.forEach(element => sum *= element)
+
+    // * for OF loop
+    // for (let element of arr) {
+    //     sum *= element
+    // }
+
+    return sum
+}
+
+console.log(multiplyArrayElements([5, 10, 3]))
