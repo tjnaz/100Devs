1d68e1b5eca2c989c46023b8f0dc7e52def41363
AnazThaj <anaz.thaj@gmail.com>
2022-08-04 14:29:34 +0530

    Code-along class-21 - objects-zeo

--- classes/class-21-materials/objects-zeo/js/main.js
+++ classes/class-21-materials/objects-zeo/js/main.js
@@ -1,1 +1,13 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    brand: 'Casio',
+    model: 'G-Shock',
+    shape: 'Round',
+    display: 'Plastic',
+    isWaterProof: false,
+    giveDescription: function () {
+        return console.log(`This is a ${this.brand} brand ${this.brand} model stopwatch. It is ${this.shape} shape, with a ${this.display} display.`)
+    }
+}
+
+stopwatch.giveDescription()
