5b0ce4eb48a97417b9b419563646486efff153c7
AnazThaj <anaz.thaj@gmail.com>
2022-08-05 19:53:43 +0530

    Practice Objects from javascript.info - codealong

---
+++ practice/06-javascript-info/3-objects-the-basics/1-objects/0-codealong/main.js
@@ -0,0 +1,24 @@
+let user = {
+    name: 'Adam',
+    age: 30,
+}
+
+user['like birds'] = true
+
+// let key = prompt('What do you want to know about the user? "name"/"age"')
+
+// *The following can't be done with the usual dot notation
+// console.log(user[key])
+
+// ^console.log(user.key) would throw an error
+
+function addUser(name, age) {
+    return { name: name, age: age, }
+}
+
+user = addUser('John', '23')
+
+console.log(user.name)
+
+// ** IN OPERATOR ** //
+console.log("age" in user)
