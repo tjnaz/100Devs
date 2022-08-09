b5fc01fbe129d5b6a2fc3d38ad1d1ff681086abd
AnazThaj <anaz.thaj@gmail.com>
2022-08-09 21:30:36 +0530

    Practice Object Reference from javascript.info - codealong - cloning and merging objects-3

--- practice/06-javascript-info/3-objects-the-basics/2-object-reference-and-copying/main.js
+++ practice/06-javascript-info/3-objects-the-basics/2-object-reference-and-copying/main.js
@@ -14,27 +14,30 @@ console.log(user.name)
 let user2 = {
     name: "John",
     age: 30
-};
+}
 
-let clone = {}; // the new empty object
+// the new empty object
+let clone = {}
 
 // let's copy all user properties into it
 for (let key in user2) {
-    clone[key] = user[key];
+    clone[key] = user[key]
 }
 
 // now clone is a fully independent object with the same content
-clone.name = "Pete"; // changed the data in it
+// changed the data in it
+clone.name = "Pete"
 
-alert(user2.name); // still John in the original object
+// still John in the original object
+alert(user2.name)
 
 
-let user3 = { name: "John" };
+let user3 = { name: "John" }
 
-let permissions1 = { canView: true };
-let permissions2 = { canEdit: true };
+let permissions1 = { canView: true }
+let permissions2 = { canEdit: true }
 
 // copies all properties from permissions1 and permissions2 into user
-Object.assign(user3, permissions1, permissions2);
+Object.assign(user3, permissions1, permissions2)
 
 // now user = { name: "John", canView: true, canEdit: true }
