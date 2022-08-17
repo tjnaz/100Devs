8f8d2624734425b3bbff8ff6feb02e57c35589cc
AnazThaj <anaz.thaj@gmail.com>
2022-08-17 21:23:33 +0530

    Practice Object - Object to Primitive Conversion

---
+++ practice/06-javascript-info/3-objects-the-basics/8-object-to-primitive-conv/main.js
@@ -0,0 +1,14 @@
+let user = {
+    name: "John",
+    money: 1000,
+
+    [Symbol.toPrimitive](hint) {
+        alert(`hint: ${hint}`);
+        return hint == "string" ? `{name: "${this.name}"}` : this.money;
+    }
+};
+
+// conversions demo:
+alert(user); // hint: string -> {name: "John"}
+alert(+user); // hint: number -> 1000
+alert(user + 500); // hint: default -> 1500
