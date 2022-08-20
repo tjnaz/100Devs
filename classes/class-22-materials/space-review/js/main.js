a9c717d35e241eaf3dcf3ada2699827211af5fb2
AnazThaj <anaz.thaj@gmail.com>
2022-08-20 21:42:56 +0530

    Code-along class-22 - space-review

---
+++ classes/class-22-materials/space-review/js/main.js
@@ -0,0 +1,51 @@
+//*Arrays
+
+//Create and array of numbers. Sum all of the numbers. Alert the sum.
+const num = [1, 2, 3, 4, 5, 6, 7]
+let sum = num.reduce((total, item) => (total + item), 0)
+
+// console.log(sum)
+
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+function squareNum(arr) {
+    let newArray = arr.map((item) => item ** 2)
+    return newArray
+}
+
+// console.log(squareNum(num))
+
+//Create a function that takes string
+//Print the reverse of that string to the console
+function reverseArray(arr) {
+    let reversedArray = arr.reverse()
+    return reversedArray
+}
+
+
+// console.log(reverseArray(squareNum(num)))
+
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
+
+function formatStr(str) {
+    return str.split(' ').join('').toLowerCase()
+}
+
+function reverseStr(str) {
+    return str.split(' ').join('').split('').reverse().join('').toLowerCase()
+}
+// console.log(formatStr('HeLlo'))
+
+function isPalindrome(str) {
+    str = formatStr(str)
+    let reversedStr = reverseStr(str)
+    for (let i = 0; i <= str.length; i++) {
+        if (str[i] !== reversedStr[i]) {
+            return console.log(`No`)
+        }
+    }
+    console.log(`Yes`)
+}
+
+isPalindrome('Rats live on no evil star')
