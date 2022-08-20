0fab2a2466ecefb2b1d3ce4a9966631a934a9735
AnazThaj <anaz.thaj@gmail.com>
2022-08-20 07:51:19 +0530

    Update minor changes to the file

--- practice/06-javascript-info/4-datatypes/5-array-methods/1-camelCase/main.js
+++ practice/06-javascript-info/4-datatypes/5-array-methods/1-camelCase/main.js
@@ -6,4 +6,4 @@ function camelize(str) {
     return (str.split('-').map((item, index) => index > 0 ? item[0].toUpperCase() + item.slice(1) : item)).join('')
 }
 
-console.log(camelize("how-are-you-wanker"))
+console.log(camelize("how-are-you-beautiful"))
