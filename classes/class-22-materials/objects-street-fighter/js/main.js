c01afbe7266aede60d32d768da24aa7d18ff17a8
AnazThaj <anaz.thaj@gmail.com>
2022-08-23 20:52:56 +0530

    Add a new property to the objects street fighter

--- classes/class-22-materials/objects-street-fighter/js/main.js
+++ classes/class-22-materials/objects-street-fighter/js/main.js
@@ -1,11 +1,12 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
 
-function CreateStreetFighter(name, age, fightingStyle) {
+function CreateStreetFighter(name, age, fightingStyle, catchPhrase) {
     this.name = name
     this.age = age
     this.fightingStyle = fightingStyle
+    this.catchPhrase = catchPhrase
 }
 
-let streetFighter1 = new CreateStreetFighter('blah', 20, 'monkey kungfu')
+let streetFighter1 = new CreateStreetFighter('blah', 20, 'monkey kungfu', `you can't handle the truth`)
 
 console.table(streetFighter1)
