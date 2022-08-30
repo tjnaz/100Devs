2a0ba5c5e66bac2a6b5c5a95cc3dd6fc85e638b6
Thaj Anaz <anaz.thaj@gmail.com>
2022-08-30 22:01:24 +0530

    Practice/finish the exercise with my solution for space-review

--- classes/class-23-materials/space-review/js/main.js
+++ classes/class-23-materials/space-review/js/main.js
@@ -1,12 +1,40 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+const arr = [1, 2, 3, 4, 5, 6, 7]
+let sum = 0
+arr.forEach(item => sum += item)
+// alert(sum)
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+function squareArr(array) {
+    return array.map(item => item * item)
+}
+
+// console.log(squareArr(arr))
 
 //Create a function that takes string
 //Print the reverse of that string to the console
+const str = 'barnaby'
 
+function reverseString(string) {
+    return string.split('').reverse().join('')
+}
+
+// console.log(reverseString(str))
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+function isPalindrome(string) {
+    const reversedString = string.split('').reverse().join('')
+
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== reversedString[i]) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPalindrome(str))
