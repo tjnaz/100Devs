67e0e73dbe95d58f37c6eef4849872f018e570c7
Thaj Anaz <anaz.thaj@gmail.com>
2022-08-31 19:48:43 +0530

    Practice/finish the exercise with my solution - tony-hawk-pro-skater

--- classes/class-23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ classes/class-23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,22 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function ProSkater(name, age, hair, nickName) {
+    this.name = name
+    this.age = age
+    this.hair = hair
+    this.nickName = nickName
+    this.hello = function() {
+        console.log(`hello skater`)
+    }
+    this.bye = function() {
+        console.log(`bye bye!`)
+    }
+    this.sup = function(){
+        console.log(`'sup homies!`)
+    }
+}
+
+const tony = new ProSkater('tony', 30, 'red', 't-bone')
+
+console.log(tony.name)
+console.log(tony.age)
