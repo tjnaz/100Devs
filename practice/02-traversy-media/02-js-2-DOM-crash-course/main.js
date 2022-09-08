aff9bbc1b83f085e9c75e14d07165731e851b0df
Thaj Anaz <anaz.thaj@gmail.com>
2022-09-08 20:04:16 +0530

    Codealong to the end of video #2

--- practice/02-traversy-media/02-js-2-DOM-crash-course/main.js
+++ practice/02-traversy-media/02-js-2-DOM-crash-course/main.js
@@ -1,13 +1,57 @@
+// ^ CLASS 01 ^ //
 // * textContent vs innerText
 
-const items = document.getElementsByClassName("list-group-item")
+// * getElementsByClassName
+// const items = document.getElementsByClassName("list-group-item")
 
-console.log(items[0])
+// console.log(items[0])
 
-items[1].style.backgroundColor = '#f4f4f4'
+// items[1].style.backgroundColor = '#f4f4f4'
 
-console.log(items[3].textContent)
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = '#f4f4f4'
+// }
 
-for (let i = 0; i < items.length; i++) {
-    items[i].style.backgroundColor = '#f4f4f4'
-}
+// *getElementsByTagName
+
+// const li = document.getElementsByTagName('li')
+
+// console.log(li)
+// li[0].style.backgroundColor = '#f5f5f5'
+
+
+// *querySelector & querySelectorAll
+
+// const firstLi = document.querySelector('.list-group-item:nth-child(1)')
+// firstLi.style.backgroundColor = 'blue'
+// const secondLi = document.querySelector('.list-group-item:nth-child(2)')
+// secondLi.style.backgroundColor = 'coral'
+
+// const items = document.querySelectorAll(".list-group-item")
+
+// items.forEach(item => item.style.backgroundColor = '#f4f4f4')
+
+
+// ^ CLASS 02 ^ //
+
+// *Traversing DOM - parentNode / parentElement
+
+// *Create a div
+
+const newDiv = document.createElement('div')
+newDiv.className = 'testClass'
+newDiv.id = 'testId'
+
+// *Set attribute
+newDiv.setAttribute('title', 'Test Title attribute')
+
+// *Create text node
+const newDivText = document.createTextNode('Hello, world!')
+
+// *Add text to the div
+newDiv.appendChild(newDivText)
+
+console.log(newDiv.textContent)
+
+
+// ^ CLASS 03 ^ //
