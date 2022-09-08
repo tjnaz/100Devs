ee8c07afe259daf76c5daf0514e9d6f506de9490
Thaj Anaz <anaz.thaj@gmail.com>
2022-09-08 14:34:03 +0530

    Codealong getElementsByClassName

--- practice/02-traversy-media/02-js-2-DOM-crash-course/main.js
+++ practice/02-traversy-media/02-js-2-DOM-crash-course/main.js
@@ -2,4 +2,12 @@
 
 const items = document.getElementsByClassName("list-group-item")
 
-console.log(items)
+console.log(items[0])
+
+items[1].style.backgroundColor = '#f4f4f4'
+
+console.log(items[3].textContent)
+
+for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = '#f4f4f4'
+}
