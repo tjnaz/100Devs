cec30a57041cc2f61e002679751524f5f933c81d
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-09 18:28:23 +0530

    Finish objects-dog-photos codealong exercise

--- classes/class-26-materials/objects-dog-photos/js/main.js
+++ classes/class-26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,12 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then((res) => res.json())
+  .then((data) => {
+    document.querySelector(
+      ".photos"
+    ).innerHTML = `<img src='${data.message}' />`;
+  })
+  .catch((err) => {
+    console.log(`Error ${err}`);
+  });
