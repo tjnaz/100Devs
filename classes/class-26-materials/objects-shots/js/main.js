a3a63e21e4b14345cd0bc6a793a49793e1f3b746
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-09 18:29:09 +0530

    Finish objects-shots API codealong exercise

--- classes/class-26-materials/objects-shots/js/main.js
+++ classes/class-26-materials/objects-shots/js/main.js
@@ -1,1 +1,11 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.drinks);
+    document.querySelector("h3").innerHTML = data.drinks[0].strDrink;
+    document.querySelector("img").src = `${data.drinks[0].strDrinkThumb}`;
+  })
+  .catch((err) => {
+    console.log(`Error ${err}`);
+  });
