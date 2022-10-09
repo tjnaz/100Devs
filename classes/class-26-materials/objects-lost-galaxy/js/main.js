fb71ae477c529ee048e4b937b89514cf3c2dce34
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-09 11:53:06 +0530

    Finish objects-lost-galaxy codealong

--- classes/class-26-materials/objects-lost-galaxy/js/main.js
+++ classes/class-26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,20 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+let pizza = {};
+
+pizza.size = "large";
+pizza.topping = "beef macaroni";
+pizza.slices = 12;
+pizza.crust = "stuffed crust";
+
+pizza.delivery = function () {
+  console.log("calculating....");
+};
+pizza.burnMouth = function () {
+  console.log("aarrgggggg!!!!!");
+};
+pizza.frisbee = function () {
+  console.log("yeeeeeertttttt!!!");
+};
+
+console.log(pizza.size);
+pizza.delivery();
