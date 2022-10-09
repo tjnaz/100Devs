f1aa2721d45f251add45d4485e15ca7726a38391
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-09 18:30:27 +0530

    Try part of turbo-review

--- classes/class-26-materials/turbo-review/js/main.js
+++ classes/class-26-materials/turbo-review/js/main.js
@@ -4,14 +4,14 @@ let positiveAffirmation = "Today is the day!";
 console.log(positiveAffirmation);
 console.log(positiveAffirmation);
 console.log(positiveAffirmation);
-console.log(positiveAffirmation);
-console.log(positiveAffirmation);
-console.log(positiveAffirmation);
-console.log(positiveAffirmation);
 
 //Declare a variable, assign it an array of letters, combine the letters into one word, and alert it
+let arrayOfLetters = ["s", "t", "f", "u"];
+let joinedArrayOfLetters = arrayOfLetters.join("");
+console.log(joinedArrayOfLetters);
 
 // *Functions*
 // Create a function that returns rock, paper, lizard, spock or scissors as randomly as possible
+
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, scissors, lizard, or spock) and determines if they won a game of rock paper scissors against a bot using the above function
