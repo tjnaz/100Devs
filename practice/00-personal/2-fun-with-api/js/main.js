14b650356a099a9ae46cbc6d33e54af5fa4759ab
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-10 06:36:44 +0530

    Finish basic logic experiments for APIs

--- practice/00-personal/2-fun-with-api/js/main.js
+++ practice/00-personal/2-fun-with-api/js/main.js
@@ -15,14 +15,49 @@ function getFetch() {
     });
 }
 
-let chapter = 2;
-let ayahN = 255;
 let apiURL = "http://api.alquran.cloud/v1/surah";
+
+document
+  .querySelector("select[name='verse']")
+  .addEventListener("change", showAyah);
+
+document
+  .querySelector("select[name='chapter']")
+  .addEventListener("change", getAyah);
+
+function getAyah(chapter) {
+  chapter = document.querySelector("select[name='chapter']").value;
+  document.querySelector("select[name='verse']").innerHTML =
+    '<option value=default">Select an Ayah</option>';
+  fetch(apiURL)
+    .then((res) => res.json())
+    .then((data) => {
+      let numberOfAyahs = data.data[chapter - 1].numberOfAyahs;
+      for (let i = 1; i <= numberOfAyahs; i++) {
+        document.querySelector(
+          "select[name='verse']"
+        ).innerHTML += `<option value=${i}>${i}</option>`;
+      }
+    });
+}
+
+function showAyah(chapter, ayah) {
+  chapter = document.querySelector("select[name='chapter']").value;
+  ayah = document.querySelector("select[name='verse']").value;
+  console.log(`${chapter} | ${ayah}`);
+}
 
 fetch(`${apiURL}`)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.data.length);
+    let surahs = data.data;
+    surahs.forEach((item) => {
+      //console.log(item.number);
+      document.querySelector(
+        "select"
+      ).innerHTML += `<option value="${item.number}">${item.number} - ${item.englishName}</option>`;
+    });
+    //console.log(data.data[0]);
   })
   .catch((err) => {
     console.log(`Error ${err}`);
