3d0fd24f76a042e8c785ae3667e2bd9680e463c4
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-19 06:55:02 +0530

    Finish object constructor - netflix show

--- classes/class-27-materials/objects-netflix-tv-shows/js/main.js
+++ classes/class-27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,27 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class CreateNetflixTVShow {
+  constructor(name, numberOfSeasons, genre, isAgeRestricted) {
+    this.name = name;
+    this.numberOfSeasons = numberOfSeasons;
+    this.isAgeRestricted = isAgeRestricted;
+  }
+  play() {
+    console.log("Playing...");
+  }
+  stop() {
+    console.log("Stopping...");
+  }
+  addToFavorites() {
+    console.log("Adding to the favorite list....");
+  }
+}
 
+let firefly = new CreateNetflixTVShow(
+  "Firefly",
+  "Space Cowboys",
+  "100%",
+  false
+);
+
+console.log(firefly);
