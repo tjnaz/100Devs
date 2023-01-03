8660bc71958f12c6e59479d084307932d47e84cf
Thaj Anaz <anaz.thaj@gmail.com>
2023-01-03 14:26:54 +0530

    Class first

--- practice/03-the-js-way/00-mega_review/proj-social_news_programs/main.js
+++ practice/03-the-js-way/00-mega_review/proj-social_news_programs/main.js
@@ -1,7 +1,13 @@
 // FizzBuzz
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
   if ((i % 5 === 0) & (i % 3 === 0)) {
-    console.log("FizzBuzz");
+    console.log(`${i}: FizzBuzz`);
+  } else if (i % 5 === 0) {
+    console.log(`${i}: Buzz`);
+  } else if (i % 3 === 0) {
+    console.log(`${i}: Fizz`);
+  } else {
+    console.log(`${i}`);
   }
 }
