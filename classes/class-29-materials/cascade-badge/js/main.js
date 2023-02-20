e1e1259c9df82608caccfc3ad4b191682f18d6c9
Thaj Anaz <anaz.thaj@gmail.com>
2023-02-20 22:32:17 +0530

    Refactor the second exercise

--- classes/class-29-materials/cascade-badge/js/main.js
+++ classes/class-29-materials/cascade-badge/js/main.js
@@ -1,7 +1,39 @@
 //Arrays
-//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console.
+
+// function reverseList(partyOrder) {
+//   let reversePartyOrder = [];
+//   for (let i = partyOrder.length - 1; i >= 0; i--) {
+//     reversePartyOrder.push(partyOrder[i]);
+//   }
+//   console.log(reversePartyOrder);
+// }
+
+function reverseList(partyOrder) {
+  console.log(partyOrder.reverse());
+}
+
+let partyOrder = ["qw:p", "snth", "xblv", "jkmz"];
+
+reverseList(partyOrder);
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+const a = [3, 2, 5, 4];
+const b = [1, 1, 1, 1];
+
+function isABiggerThanB(a, b) {
+  const sumA = a.reduce((result, item) => {
+    return result + item * item;
+  }, 0);
+  const sumB = b.reduce((result, item) => {
+    return result + item * item * item;
+  }, 0);
+  console.log(`${sumA}, ${sumB}`);
+  return sumA > sumB ? true : false;
+}
+
+console.log(`Is array A bigger than array B? ${isABiggerThanB(a, b)}`);
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
