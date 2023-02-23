ebea3107052fbd4d8db4a90af38f02b0f201ac0b
Thaj Anaz <anaz.thaj@gmail.com>
2023-02-23 11:07:13 +0530

    Finish the first exercise - class constructor

--- classes/class-30-materials/class30-materials/objects-espresso-machine/js/main.js
+++ classes/class-30-materials/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,21 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+  constructor(color, make, model, price) {
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.price = price;
+  }
+  turnOn() {
+    console.log("ON!");
+  }
+  steam() {
+    console.log("STEAM!!");
+  }
+  brew() {
+    console.log("IT'S BREWING TIME!!");
+  }
+}
+
+let gaggia = new EspressoMachine("red", "Gaggia", "Classic Pro", 400);
