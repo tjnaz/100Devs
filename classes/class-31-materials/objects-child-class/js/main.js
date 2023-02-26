3e9fd5c8bebf081f73857ff072c8baf4cce89a01
Thaj Anaz <anaz.thaj@gmail.com>
2023-02-26 23:32:38 +0530

    Create Cat class extending the Animal class

--- classes/class-31-materials/objects-child-class/js/main.js
+++ classes/class-31-materials/objects-child-class/js/main.js
@@ -16,4 +16,27 @@ class Dog extends Animal {
   }
 }
 
+class GuardDog extends Dog {
+  constructor(name, breed, hunt) {
+    super(name, breed);
+    this.hunt = hunt;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this._breed = breed;
+  }
+  get breed() {
+    return this._breed;
+  }
+  speak() {
+    super.speak();
+    console.log(`${this.name} meows`);
+  }
+}
+
 let simba = new Dog("Simba", "Sheperd");
+let wolf = new GuardDog("Loney", "Wolf", "Lions");
+let polly = new Cat("Polly", "Street");
