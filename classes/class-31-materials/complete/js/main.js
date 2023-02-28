ccacb96012fd1d848664c21902ec5a72eef3b686
Thaj Anaz <anaz.thaj@gmail.com>
2023-02-28 19:32:24 +0530

    Finish class 31

--- classes/class-31-materials/complete/js/main.js
+++ classes/class-31-materials/complete/js/main.js
@@ -1,47 +1,49 @@
-class Animal{
-    constructor(name){
-        this._name = name
-    }
-    get name(){
-        return this._name
-    }
-    speak(){
-        console.log(`${this._name} makes a sound`)
-    }
-}
-class Dog extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} barks`)
-    }    
-}
-class Cat extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} meows`)
-    }    
+class Animal {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  speak() {
+    console.log(`${this._name} makes a sound`);
+  }
 }
 
-let simba = new Dog('Simba','Shepard')
-let machi = new Dog('The Machine','Pitbull')
-let salem = new Cat('Salem', 'American Shorthair')
+class Dog extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this._breed = breed;
+  }
+  get breed() {
+    return this._breed;
+  }
+  speak() {
+    super.speak();
+    console.log(`${this.name} barks`);
+  }
+}
 
-let farm = [simba,machi,salem]
+class Cat extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this._breed = breed;
+  }
+  get breed() {
+    return this._breed;
+  }
+  speak() {
+    super.speak();
+    console.log(`${this.name} meows`);
+  }
+}
 
-for( a of farm ){
-    a.speak()
+let simba = new Dog("Simba", "Shepard");
+let machi = new Dog("The Machine", "Pitbull");
+let salem = new Cat("Salem", "American Shorthair");
+
+let farm = [simba, machi, salem];
+
+for (a of farm) {
+  a.speak();
 }
