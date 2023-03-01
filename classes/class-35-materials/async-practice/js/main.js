73b1521d6904ef78e8e78fb2159004195d2ba0df
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-01 15:57:46 +0530

    Try code 6

--- classes/class-35-materials/async-practice/js/main.js
+++ classes/class-35-materials/async-practice/js/main.js
@@ -69,34 +69,34 @@
 // promise.then((data) => console.log(data)).catch((err) => console.log(err));
 
 //Code 06
-// function houseOne(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
-//     })
-// }
-// function houseTwo(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
-// }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
-//         }, 2000)
-//     })
-// }
-// houseOne()
-//     .then(data => console.log(data))
-//     .then(houseTwo)
-//     .then(data => console.log(data))
-//     .then(houseThree)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+function houseOne() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 1");
+    }, 1000);
+  });
+}
+function houseTwo() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 2");
+    }, 5000);
+  });
+}
+function houseThree() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 3");
+    }, 2000);
+  });
+}
+houseOne()
+  .then((data) => console.log(data))
+  .then(houseTwo)
+  .then((data) => console.log(data))
+  .then(houseThree)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
 
 //Code 07
 // function houseOne(){
