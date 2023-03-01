b9887bc1c7bf18998a84000c10605a67b4b82676
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-01 15:48:09 +0530

    Finish class 32

--- classes/class-32-materials/complete/js/main.js
+++ classes/class-32-materials/complete/js/main.js
@@ -37,58 +37,50 @@
 //     }
 // }
 
-
-
-
-
-
-
-
-
-class Animal{
-    constructor(name){
-        this._name = name
-    }
-    get name(){
-        return this._name
-    }
-    speak(){
-        console.log(`${this._name} makes a sound`)
-    }
+class Animal {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  speak() {
+    console.log(`${this._name} makes a sound`);
+  }
 }
-class Dog extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} barks`)
-    }    
+class Dog extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this._breed = breed;
+  }
+  get breed() {
+    return this._breed;
+  }
+  speak() {
+    super.speak();
+    console.log(`${this.name} barks`);
+  }
 }
-class Cat extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} meows`)
-    }    
+class Cat extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this._breed = breed;
+  }
+  get breed() {
+    return this._breed;
+  }
+  speak() {
+    super.speak();
+    console.log(`${this.name} meows`);
+  }
 }
 
-let simba = new Dog('Simba','Shepard')
-let machi = new Dog('The Machine','Pitbull')
-let salem = new Cat('Salem', 'American Shorthair')
+let simba = new Dog("Simba", "Shepard");
+let machi = new Dog("The Machine", "Pitbull");
+let salem = new Cat("Salem", "American Shorthair");
 
-let farm = [simba,machi,salem]
+let farm = [simba, machi, salem];
 
-for( a of farm ){
-    a.speak()
+for (a of farm) {
+  a.speak();
 }
