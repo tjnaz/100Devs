6f954a3c2c05f9a6ecfd05e317aa20eaab767757
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-03 14:24:58 +0530

    Finish temperature conversion

--- practice/00-personal/3-js_review/super-review/temperature-converter/js/main.js
+++ practice/00-personal/3-js_review/super-review/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+// C to F
+
+document.querySelector("h1").addEventListener("click", cToF);
+
+function cToF() {
+  let temp = document.querySelector("input").value;
+
+  temp = temp * 1.8 + 32;
+
+  document.querySelector("h2").innerText = temp;
+}
