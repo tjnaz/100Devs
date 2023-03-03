992451db353b37a373f3ce4be21e833ab60a7610
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-03 16:49:25 +0530

    Finish listen to your heart - object

--- practice/00-personal/3-js_review/super-review/objects-listen-to-your-heart/js/main.js
+++ practice/00-personal/3-js_review/super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,16 @@
 //Create a stopwatch object that has four properties and three methods
+const stopwatch = {};
+
+stopwatch.brand = "Casio";
+stopwatch.color = "Black";
+stopwatch.age = 5;
+stopwatch.owner = "Me";
+stopwatch.start = function () {
+  console.log("STARTING!!!");
+};
+stopwatch.stop = function () {
+  console.log("STOP!!!!");
+};
+stopwatch.tellTime = function () {
+  console.log("It's time");
+};
