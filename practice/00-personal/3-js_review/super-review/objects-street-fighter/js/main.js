6037ea645609a8d3eae549f2bfb951e63e891f59
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-03 17:22:18 +0530

    Implement with constructor method

--- practice/00-personal/3-js_review/super-review/objects-street-fighter/js/main.js
+++ practice/00-personal/3-js_review/super-review/objects-street-fighter/js/main.js
@@ -1,1 +1,19 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function MakeStreetFighter(name, age, tribe, height) {
+  this.name = name;
+  this.age = age;
+  this.tribe = tribe;
+  this.height = height;
+  this.yell = function () {
+    alert("HEEYA!!");
+  };
+  this.punch = function () {
+    alert("POW!!");
+  };
+  this.kick = function () {
+    alert("YEEHA!!");
+  };
+}
+
+let leon = new MakeStreetFighter("Leon", 30, "#100Devs", "6ft");
