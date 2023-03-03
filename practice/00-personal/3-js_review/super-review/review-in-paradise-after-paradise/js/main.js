dfe65abb1183bb9e3e9baccd5116fc0244c64e3f
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-03 16:22:55 +0530

    Finish review in paradise after paradise

--- practice/00-personal/3-js_review/super-review/review-in-paradise-after-paradise/js/main.js
+++ practice/00-personal/3-js_review/super-review/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,15 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function evalArray(arr) {
+  const first = arr[0];
+  const last = arr[arr.length - 1];
+
+  if (first < last) {
+    console.log("Hi");
+  } else if (first > last) {
+    console.log("Bye");
+  } else {
+    console.log("We close in an hour");
+  }
+}
+
+evalArray([7, 7]);
