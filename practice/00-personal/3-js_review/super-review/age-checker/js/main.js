dd6712c521b68adcb37cb7a6d6e383f8e359a9b3
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-03 10:11:09 +0530

    Finish age checker

--- practice/00-personal/3-js_review/super-review/age-checker/js/main.js
+++ practice/00-personal/3-js_review/super-review/age-checker/js/main.js
@@ -6,8 +6,26 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
-
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+document.querySelector("h1").addEventListener("click", checkAge);
+const p = document.querySelector("p");
+
+function checkAge() {
+  let age = document.querySelector("#danceDanceRevolution").value;
+
+  if (age < 16) {
+    p.innerText = "Can't drive";
+  } else if (age < 18) {
+    p.innerText = "can't hate from outside the club";
+  } else if (age < 21) {
+    p.innerText = "can not drink";
+  } else if (age < 25) {
+    p.innerText = "can't rent";
+  } else {
+    p.innerText = "Too old for this stuff";
+  }
+}
