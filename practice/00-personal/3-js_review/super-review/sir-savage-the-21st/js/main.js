669b6be91e9cc66daec4a97e0eab14c43fd2df00
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-03 15:27:25 +0530

    Finish 21 loops

--- practice/00-personal/3-js_review/super-review/sir-savage-the-21st/js/main.js
+++ practice/00-personal/3-js_review/super-review/sir-savage-the-21st/js/main.js
@@ -1,2 +1,10 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+function loop21() {
+  for (let i = 0; i < 21; i++) {
+    console.log(21);
+    document.querySelector("body").innerHTML += "<p>21</p>";
+  }
+}
+loop21();
+
 //Bonus can you make it print '21' 21 times to the dom?
