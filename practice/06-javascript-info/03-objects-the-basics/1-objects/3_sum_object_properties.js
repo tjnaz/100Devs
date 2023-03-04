caa9d72e611d686caa5d906fecaf46934d0c8470
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-04 16:08:10 +0530

    Complete the summing of object properties

---
+++ practice/06-javascript-info/03-objects-the-basics/1-objects/3_sum_object_properties.js
@@ -0,0 +1,26 @@
+// https://javascript.info/object#sum-object-properties
+
+// Sum object properties
+// importance: 5
+
+// We have an object storing salaries of our team:
+
+let salaries = {
+  john: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+// If salaries is empty, then the result must be 0.
+
+function sumObjProp(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    sum += obj[key];
+  }
+  return sum;
+}
+
+console.log(sumObjProp(salaries));
