cfa9dac4a384fee37c4f2582cbcc61f40c0df690
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-04 14:07:38 +0530

    Complete arrow function challenge

---
+++ practice/06-javascript-info/01-js-fundamentals/17-arrow_functions/1_rewrite_arrow_func.js
@@ -0,0 +1,26 @@
+// https://javascript.info/arrow-functions-basics#rewrite-with-arrow-functions
+
+// Rewrite with arrow functions
+
+// Replace Function Expressions with arrow functions in the code below:
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// ask(
+//   "Do you agree?",
+//   function() { alert("You agreed."); },
+//   function() { alert("You canceled the execution."); }
+// );
+
+let ask = (question, yes, no) => {
+  confirm(question) ? yes() : no();
+};
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
