b20de1a984c17bc629e38298efb4a2be44932a8f
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-05 20:44:04 +0530

    Complete [05/13] of Array Methods - wow arr.slice just so elegant

--- practice/06-javascript-info/04-datatypes/5-array-methods/5_copy_and_sort_array.js
+++ practice/06-javascript-info/04-datatypes/5-array-methods/5_copy_and_sort_array.js
@@ -1,10 +1,45 @@
 function copySorted(arr) {
-    return arr.slice().sort()
+  return arr.slice().sort();
 }
 
-let arr = ['I', 'learn', 'fix', 'excel', 'win', 'study', 'complex', 'language', 'study']
+// let arr = ['I', 'learn', 'fix', 'excel', 'win', 'study', 'complex', 'language', 'study']
 
-let sorted = copySorted(arr)
+// let sorted = copySorted(arr)
 
-console.log(sorted)
-console.log(arr)
+// console.log(sorted)
+// console.log(arr)
+
+// --- ************ --- //
+// --- [2023-03-05] --- //
+// --- ************ --- //
+
+// https://javascript.info/array-methods#copy-and-sort-array
+
+// Copy and sort array
+// importance: 5
+
+// We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
+
+// Create a function copySorted(arr) that returns such a copy.
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (no changes)
+
+// function copySorted(arr) {
+//   let sortedArray = [];
+//   arr.forEach((e) => {
+//     sortedArray.push(e);
+//   });
+//   return sortedArray.sort();
+// }
+
+// THIS IS JUST AWESOME !!!
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+let sorted = copySorted(arr);
+console.log(arr);
+console.log(sorted);
