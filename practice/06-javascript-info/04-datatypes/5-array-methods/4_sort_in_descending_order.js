6cd1a07f4a2ae1b7224a0bfc944ec52151a4b1ab
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-05 20:20:51 +0530

    Complete [04/13] of Array Methods (kinda easy)

--- practice/06-javascript-info/04-datatypes/5-array-methods/4_sort_in_descending_order.js
+++ practice/06-javascript-info/04-datatypes/5-array-methods/4_sort_in_descending_order.js
@@ -1,9 +1,26 @@
-let arr = [10, 15, 245, 412, 7, 97, 16, 100, 150, 50]
+// let arr = [10, 15, 245, 412, 7, 97, 16, 100, 150, 50]
 
-// *MY SOLUTION* //
-arr.sort((a, b) => a - b).reverse()
-console.log(arr)
+// // *MY SOLUTION* //
+// arr.sort((a, b) => a - b).reverse()
+// console.log(arr)
 
-// *GIVEN SOLUTION* //
+// // *GIVEN SOLUTION* //
 
-// console.log(arr.sort((a, b) => b - a))
+// // console.log(arr.sort((a, b) => b - a))
+
+// --- ************ --- //
+// --- [2023-03-05] --- //
+// --- ************ --- //
+
+// https://javascript.info/array-methods#sort-in-decreasing-order
+
+// Sort in decreasing order
+// importance: 4
+
+let arr = [5, 2, 1, -10, 8];
+
+// // ... your code to sort it in decreasing order
+
+// alert( arr ); // 8, 5, 2, 1, -10
+
+console.log(arr.sort((a, b) => b - a));
