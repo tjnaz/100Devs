8b4c1cbf41794932e5ed41eb756147d729c152aa
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-05 18:28:35 +0530

    Complete [03/13] of Array Methods

--- practice/06-javascript-info/04-datatypes/5-array-methods/3_filter_range_in_place.js
+++ practice/06-javascript-info/04-datatypes/5-array-methods/3_filter_range_in_place.js
@@ -1,15 +1,42 @@
+// // ! forEach is skipping an index after removing it.
+// function filterRangeInPlace(arr, a, b) {
+//     for (i = 0; i < arr.length; i++) {
+//         if (arr[i] < a || arr[i] > b) {
+//             arr.splice(i, 1)
+//             i--
+//         }
+//     }
+// }
 
-// ! forEach is skipping an index after removing it.
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+// filterRangeInPlace(arr, 3, 7)
+
+// console.log(arr)
+
+// --- ************ --- //
+// --- [2023-03-05] --- //
+// --- ************ --- //
+
+// https://javascript.info/array-methods#filter-range-in-place
+
+// Filter range "in place"
+// importance: 4
+
+// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+
+// The function should only modify the array. It should not return anything.
+
+// For instance:
+
+let arr = [5, 3, 8, 1];
+
+// filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+// alert( arr ); // [3, 1]
+
 function filterRangeInPlace(arr, a, b) {
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i] < a || arr[i] > b) {
-            arr.splice(i, 1)
-            i--
-        }
-    }
+  return arr.map((e, i) => e >= a && e <= b ? e : arr.splice(i, 1));
 }
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8]
-filterRangeInPlace(arr, 3, 7)
-
-console.log(arr)
+filterRangeInPlace(arr, 1, 4);
+console.log(arr);
