c04a9dbcd81187c613720fb2b71641d4d10a469f
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-06 13:14:00 +0530

    Comment out the Previous Attempt

--- practice/04-eloquent-js/ch4-ds-obj-and-arr/2_reversing_an_array.js
+++ practice/04-eloquent-js/ch4-ds-obj-and-arr/2_reversing_an_array.js
@@ -1,37 +1,37 @@
-let arr = [1, 2, 3, 4, 5]
-let arr2 = []
-let arrayLength = arr.length
+// let arr = [1, 2, 3, 4, 5]
+// let arr2 = []
+// let arrayLength = arr.length
 
+// // ^ Apprantly can't use the built-in reverse function
+// // function reverseArray(arr) {
+// //     let arr2 = arr.slice()
+// //     return arr2.reverse()
+// // }
 
-// ^ Apprantly can't use the built-in reverse function
+// // function reverseArrayInPlace(arr) {
+// //     return arr = arr.reverse()
+// // }
+
 // function reverseArray(arr) {
-//     let arr2 = arr.slice()
-//     return arr2.reverse()
+//     for (i = 0; i < arr.length; i++) {
+//         arr2.unshift(arr[i])
+//     }
+//     return arr2
 // }
 
 // function reverseArrayInPlace(arr) {
-//     return arr = arr.reverse()
+//     arr.forEach((element, index) => {
+//         arr.unshift(arr[index * 2])
+//     })
+//     arr.splice(arrayLength, arrayLength)
+//     return arr
 // }
 
+// console.log(reverseArray(arr))
+// console.log(arr)
+// console.log(reverseArrayInPlace(arr))
+// console.log(arr)
 
-
-function reverseArray(arr) {
-    for (i = 0; i < arr.length; i++) {
-        arr2.unshift(arr[i])
-    }
-    return arr2
-}
-
-function reverseArrayInPlace(arr) {
-    arr.forEach((element, index) => {
-        arr.unshift(arr[index * 2])
-    })
-    arr.splice(arrayLength, arrayLength)
-    return arr
-}
-
-
-console.log(reverseArray(arr))
-console.log(arr)
-console.log(reverseArrayInPlace(arr))
-console.log(arr)
+// --- ************ --- //
+// --- [2023-03-06] --- //
+// --- ************ --- //
