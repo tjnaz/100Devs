81b4a9f1bb716705bcac6e421548063d17c207f8
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-08 12:40:39 +0530

    Add comments with the task

--- practice/06-javascript-info/03-objects-the-basics/1-objects/1_hello_objects.js
+++ practice/06-javascript-info/03-objects-the-basics/1-objects/1_hello_objects.js
@@ -12,6 +12,19 @@
 // --- [2023-03-08] --- //
 // --- ************ --- //
 
+// https://javascript.info/object#hello-object
+
+// Hello, object
+// importance: 5
+
+// Write the code, one line for each action:
+
+//     Create an empty object user.
+//     Add the property name with the value John.
+//     Add the property surname with the value Smith.
+//     Change the value of the name to Pete.
+//     Remove the property name from the object.
+
 let user = {};
 
 user.name = "John";
