4f3d5f6b6a8f56804ac31ad2fac0c741ef9871e9
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-08 12:47:52 +0530

    Complete [02/04] of Objects from JS Info (GOT IN THE FIRST TRY!)

--- practice/06-javascript-info/03-objects-the-basics/1-objects/2_check_for_emptiness.js
+++ practice/06-javascript-info/03-objects-the-basics/1-objects/2_check_for_emptiness.js
@@ -24,15 +24,45 @@
 //   return objArr.length > 0 ? false : true;
 // }
 
-let isEmpty = (obj) => {
+// let isEmpty = (obj) => {
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return true;
+// };
+
+// let obj = {};
+// // obj.name = "bike";
+// // obj.age = 10;
+
+// console.log(isEmpty(obj));
+
+// --- ************ --- //
+// --- [2023-03-08] --- //
+// --- ************ --- //
+
+// https://javascript.info/object#check-for-emptiness
+
+// Check for emptiness
+// importance: 5
+
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+// Should work like that:
+
+let schedule = {};
+
+// alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) ); // false
+
+function isEmpty(obj) {
   for (let key in obj) {
     return false;
   }
   return true;
-};
+}
 
-let obj = {};
-// obj.name = "bike";
-// obj.age = 10;
-
-console.log(isEmpty(obj));
+console.log(isEmpty(schedule));
