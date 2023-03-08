6cabe111bf60cee622dc1933466f1f8a270a83cc
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-08 14:25:38 +0530

    Complete [01/03] of Object Methods from JS Info

---
+++ practice/06-javascript-info/03-objects-the-basics/4-object-methods-1-this/1_using_this_in_obj_literal.js
@@ -0,0 +1,23 @@
+// --- ************ --- //
+// --- [2023-03-08] --- //
+// --- ************ --- //
+
+// https://javascript.info/object-methods#using-this-in-object-literal
+
+// Using "this" in object literal
+// importance: 5
+
+// Here the function makeUser returns an object.
+
+// What is the result of accessing its ref? Why?
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this,
+  };
+}
+
+let user = makeUser();
+
+console.log(user.ref.name); // What's the result? // Error: Cannot read property 'name' of undefined
