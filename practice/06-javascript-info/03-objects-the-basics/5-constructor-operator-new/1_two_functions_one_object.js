73be22ae59f145909204033ab79b696af6db4441
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-09 15:31:23 +0530

    Learnt some new and interesting things

---
+++ practice/06-javascript-info/03-objects-the-basics/5-constructor-operator-new/1_two_functions_one_object.js
@@ -0,0 +1,36 @@
+// --- ************ --- //
+// --- [2023-03-09] --- //
+// --- ************ --- //
+
+// https://javascript.info/constructor-new#two-functions--one-object
+// Two functions – one object
+// importance: 2
+
+// Is it possible to create functions A and B so that new A() == new B()?
+
+// function A() { ... }
+// function B() { ... }
+
+// let a = new A();
+// let b = new B();
+
+// alert( a == b ); // true
+
+// If it is, then provide an example of their code.
+
+// Hmm it's possible.
+
+// If a function returns an object then new returns it instead of this.
+
+// So they can, for instance, return the same externally defined object obj:
+
+let obj = {};
+
+function A() {
+  return obj;
+}
+function B() {
+  return obj;
+}
+
+console.log(new A() == new B()); // true
