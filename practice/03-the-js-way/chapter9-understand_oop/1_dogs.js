31192e1a44f3b8b27b267a399bb8b2cd17b3638f
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-10 19:43:38 +0530

    Complete [01/03] of Understand OOP from JS Way

---
+++ practice/03-the-js-way/chapter9-understand_oop/1_dogs.js
@@ -0,0 +1,39 @@
+// --- ************ --- //
+// --- [2023-03-10] --- //
+// --- ************ --- //
+
+// https://github.com/thejsway/thejsway/blob/master/manuscript/chapter09.md#dogs
+// Dogs
+
+// Complete the following program to add the definition of the Dog class.
+
+//     Dogs taller than 60 emote "Grrr! Grrr!" when they bark, other ones yip "Woof! Woof!".
+
+// // TODO: define the Dog class here
+
+// const fang = new Dog("Fang", "boarhound", 75);
+// console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+// console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+// const snowy = new Dog("Snowy", "terrier", 22);
+// console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+// console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
+
+class Dog {
+  constructor(name, species, size) {
+    this.name = name;
+    this.species = species;
+    this.size = size;
+  }
+  bark() {
+    return this.size > 60 ? `Grrr! Grrr!` : `Woof! Woof!`;
+  }
+}
+
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
