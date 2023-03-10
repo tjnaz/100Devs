5654cf93cf23e690abfa350457cd7c50dd268f77
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-10 23:10:29 +0530

    Format

--- practice/04-eloquent-js/ch3-functions/1_minimum.js
+++ practice/04-eloquent-js/ch3-functions/1_minimum.js
@@ -1,4 +1,4 @@
-const min = (num1, num2) => num1 > num2 ? num2 : num1
+const min = (num1, num2) => (num1 > num2 ? num2 : num1);
 
-console.log(min(0, 10))
-console.log(min(40, 10))
+console.log(min(0, 10));
+console.log(min(40, 10));
