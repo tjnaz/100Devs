c895e0950a436853029db9a980c93239fd6f7a21
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-10 18:11:26 +0530

    Add attack method to the class

--- practice/03-the-js-way/chapter9-understand_oop/2_character_inventory.js
+++ practice/03-the-js-way/chapter9-understand_oop/2_character_inventory.js
@@ -1,4 +1,3 @@
-console.log("hello");
 // --- ************ --- //
 // --- [2023-03-10] --- //
 // --- ************ --- //
@@ -29,6 +28,30 @@ class Character {
     this.xp = 0; // XP is always 0 for new characters
   }
 
+  // Attack a target <- "attack()" method takes "target" as the argument
+  attack(target) {
+    if (this.health > 0) {
+      const damage = this.strength;
+      console.log(
+        `${this.name} attacks ${target.name} and causes ${damage} damage points`,
+      );
+      target.health -= damage;
+
+      if (target.health > 0) {
+        console.log(`${target.name} has ${target.health} health points left`);
+      } else {
+        target.health = 0;
+        const bonusXP = 10;
+        console.log(
+          `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points`,
+        );
+        this.xp += bonusXP;
+      }
+    } else {
+      console.log(`${this.name} can't attack (they've been eliminated)`);
+    }
+  }
+
   // Return the character description
   describe() {
     return `${this.name} has ${this.health} health points, ${this.strength} as strength, and ${this.xp} XP points`;
@@ -36,7 +59,19 @@ class Character {
 }
 
 const aurora = new Character("Aurora", 150, 25);
-const glacious = new Character("Glacious", 130, 30);
+const glacius = new Character("Glacius", 130, 30);
+
+console.log("Welcome to the adventure! Here are our heroes:");
+console.log(aurora.describe());
+console.log(glacius.describe());
+
+const monster = new Character("Spike", 40, 20);
+console.log("A wild monster has appeared: it's named " + monster.name);
+
+monster.attack(aurora);
+monster.attack(glacius);
+aurora.attack(monster);
+glacius.attack(monster);
 
 console.log(aurora.describe());
-console.log(glacious.describe());
+console.log(glacius.describe());
