db45210c29f0b64680664e6060e7895e8452af34
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-12 17:35:07 +0530

    Remove unnecessary lines

--- practice/04-eloquent-js/ch3-functions/2_recursion.js
+++ practice/04-eloquent-js/ch3-functions/2_recursion.js
@@ -1,20 +1,15 @@
 // TODO I want to find out how to display the original number along with whether it's odd or even
-// let originalNum = 0
 function isEven(num) {
-    if (num === 0) {
-        return true
-        // return console.log(`${originalNum} is even`)
-    } else if (num === 1) {
-        return false
-        // return console.log(`${originalNum} is odd`)
-    } else if (num < 0) {
-        return `Only whole numbers are acceptable`
-        // return console.log(`${originalNum} is not a whole number`)
-    }
-    return isEven(num - 2)
-    // originalNum = num + 2
+  if (num === 0) {
+    return true;
+  } else if (num === 1) {
+    return false;
+  } else if (num < 0) {
+    return `Only whole numbers are acceptable`;
+  }
+  return isEven(num - 2);
 }
 
-console.log(isEven(50))
-console.log(isEven(75))
-console.log(isEven(-1))
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
