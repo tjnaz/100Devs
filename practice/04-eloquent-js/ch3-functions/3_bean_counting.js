c847da63953e40d20abc1b275baa0a87cb10ef39
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-12 20:59:20 +0530

    Modify the code a little bit

--- practice/04-eloquent-js/ch3-functions/3_bean_counting.js
+++ practice/04-eloquent-js/ch3-functions/3_bean_counting.js
@@ -1,24 +1,26 @@
 function countBs(text) {
-    let numOfBs = 0
-    for (let i = 0; i <= text.length; i++) {
-        if (text[i] === 'B') {
-            numOfBs += 1
-        }
+  let numOfBs = 0;
+  for (let i = 0; i <= text.length; i++) {
+    if (text[i] === "B") {
+      numOfBs += 1;
     }
-    return numOfBs
+  }
+  return numOfBs;
 }
 
 function countChar(text, charToBeCounted) {
-    let numOfChars = 0
-    for (let i = 0; i <= text.length; i++) {
-        if (text[i] === charToBeCounted) {
-            numOfChars += 1
-        }
-    }
-    return numOfChars
+  let numOfChars = 0;
+  for (let i = 0; i <= text.length; i++) {
+    // if (text[i] === charToBeCounted) {
+    //     numOfChars += 1
+    // }
+    // A little modification
+    text[i] === charToBeCounted && numOfChars++;
+  }
+  return numOfChars;
 }
 
-console.log(countBs("Blah Blah Blah"))
-console.log(countChar("aleena", "a"))
-console.log(countChar("anas", "a"))
-console.log(countChar("maryam", "a"))
+console.log(countBs("Blah Blah Blah"));
+console.log(countChar("aleena", "a"));
+console.log(countChar("anas", "a"));
+console.log(countChar("maryam", "a"));
