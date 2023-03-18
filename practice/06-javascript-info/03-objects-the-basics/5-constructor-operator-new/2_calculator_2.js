6dbc73b20d22f17f525fcbd6f6a7810d300f295f
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-18 15:18:26 +0530

    Revisit the objects topic from JS Info

--- practice/06-javascript-info/03-objects-the-basics/5-constructor-operator-new/2_calculator_2.js
+++ practice/06-javascript-info/03-objects-the-basics/5-constructor-operator-new/2_calculator_2.js
@@ -2,6 +2,25 @@
 // --- [2023-03-09] --- //
 // --- ************ --- //
 
+// Create new Calculator
+// https://javascript.info/constructor-new#create-new-calculator
+// importance: 5
+
+// Create a constructor function Calculator that creates objects with 3 methods:
+
+// read() prompts for two values and saves them as object properties with names a and b respectively.
+// sum() returns the sum of these properties.
+// mul() returns the multiplication product of these properties.
+
+// For instance:
+
+// let calculator = new Calculator();
+// calculator.read();
+
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
+
+// <<<<<<< SOLUTION >>>>>>> //
 const prompt = require("prompt-sync")();
 
 // let calculator = {
