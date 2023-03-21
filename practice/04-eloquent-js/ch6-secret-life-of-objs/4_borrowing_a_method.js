19373610afce2308bb7f9435043c43c531f4027b
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-21 17:11:43 +0530

    Complete [04/04] of ch 6 from Eloquent JS

---
+++ practice/04-eloquent-js/ch6-secret-life-of-objs/4_borrowing_a_method.js
@@ -0,0 +1,17 @@
+// --- ************ --- //
+// --- [2023-03-21] --- //
+// --- ************ --- //
+
+// Borrowing a method
+// https://eloquentjavascript.net/06_object.html#i_wcWSnr9zHV
+
+// Earlier in the chapter I mentioned that an object’s hasOwnProperty can be used as a more robust alternative to the in operator when you want to ignore the prototype’s properties. But what if your map needs to include the word "hasOwnProperty"? You won’t be able to call that method anymore because the object’s own property hides the method value.
+
+// Can you think of a way to call hasOwnProperty on an object that has its own property by that name?
+
+let map = { one: true, two: true, hasOwnProperty: true };
+
+// Fix this call
+console.log(hasOwnProperty.call(map, "one"));
+// console.log(map.hasOwnProperty("one"));
+// → true
