569d1ed485abf3507f7627d3900bd2fdca6ba689
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-24 20:19:42 +0530

    redo: practice func 5

--- practice/03-the-js-way/chapter5-functions/5-circumferance_and_area_of_a_circle.js
+++ practice/03-the-js-way/chapter5-functions/5-circumferance_and_area_of_a_circle.js
@@ -1,7 +1,28 @@
-const areaOfACircle = radius => Math.floor(Math.PI * (radius * radius))
-const circumferanceOfACircle = radius => Math.floor(2 * Math.PI * radius)
+// const areaOfACircle = radius => Math.floor(Math.PI * (radius * radius))
+// const circumferanceOfACircle = radius => Math.floor(2 * Math.PI * radius)
 
-console.log('Area of a Circle with the radius of 7')
-console.log(areaOfACircle(7))
-console.log('Circumferance of a Circle with the radius of 7')
-console.log(circumferanceOfACircle(7))
+// console.log('Area of a Circle with the radius of 7')
+// console.log(areaOfACircle(7))
+// console.log('Circumferance of a Circle with the radius of 7')
+// console.log(circumferanceOfACircle(7))
+
+// -- ************ --- //
+// --- [2023-04-24] --- //
+// --- ************ --- //
+
+// Circumference and area of a circle
+// Write a program containing two functions to calculate the circumference and area of a circle defined by its radius. Test it using user input.
+
+// Here are some tips for solving this exercise:
+
+//     Circumference and area calculation formulas should be part of your secondary school memories... Or a Google click away :)
+//     The value of number π (Pi) is obtained with Math.PI in JavaScript.
+//     You might want to use the exponentiation operator ** to perform computations.
+// console.log(2 ** 3); // 8: 2 * 2 * 2
+// console.log(3 ** 2); // 9: 3 * 3
+
+const areaOfACircle = (r) => Math.floor(Math.PI * r ** 2);
+const circumferanceOfACircle = (r) => Math.floor(2 * Math.PI * r);
+
+console.log(areaOfACircle(7));
+console.log(circumferanceOfACircle(7));
