974895da00920cf38b481f0750e54dbd0c6a16fb
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-24 19:59:22 +0530

    redo: practice

--- practice/03-the-js-way/chapter5-functions/1-improved_hello.js
+++ practice/03-the-js-way/chapter5-functions/1-improved_hello.js
@@ -1,8 +1,33 @@
+// function sayHello(firstName, lastName) {
+//     return `Hello, ${firstName} ${lastName}`
+// }
+
+// const firstName = prompt('Enter your first name:')
+// const lastName = prompt('Enter your last name:')
+
+// alert(sayHello(firstName, lastName))
+//
+
+// -- ************ --- //
+// --- [2023-04-24] --- //
+// --- ************ --- //
+
+// Improved hello
+
+// Complete the following program so that it asks the user for his first and last names, then show the result of the sayHello() function.
+
+// // Say hello to the user
+// function sayHello(firstName, lastName) {
+//   const message = `Hello, ${firstName} ${lastName}!`;
+//   return message;
+// }
+
+// // TODO: ask user for first and last name
+// // TODO: call sayHello() and show its result
+
 function sayHello(firstName, lastName) {
-    return `Hello, ${firstName} ${lastName}`
+  const message = `Hello, ${firstName} ${lastName}!`;
+  return message;
 }
 
-const firstName = prompt('Enter your first name:')
-const lastName = prompt('Enter your last name:')
-
-alert(sayHello(firstName, lastName))
+console.log(sayHello("tj", "naz"));
