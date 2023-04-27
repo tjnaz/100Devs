32b6ff690473f1b5e1bb273f07024be7902cee86
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-27 16:21:59 +0530

    fix: file names

--- practice/03-the-js-way/chapter6-objects/1-adding_character_experience.js
+++ practice/03-the-js-way/chapter6-objects/1-adding_character_experience.js
@@ -1,3 +1,33 @@
+// let aurora = {
+//     name: 'Aurora',
+//     health: 150,
+//     strength: 25,
+//     xp: 0,
+//     describe() {
+//         return `${this.name} has ${this.health} health points, ${this.strength} as strength, and ${this.xp} XP points.`
+//     }
+// }
+
+// // Aurora is harmed by an arrow
+// aurora.health -= 20;
+
+// // Aurora equips a strength necklace
+// aurora.strength += 10;
+
+// // Aurora learnt a new skill
+// aurora.xp += 15
+
+// console.log(aurora.describe())
+
+
+// -- ************ --- //
+// --- [2023-04-24] --- //
+// --- ************ --- //
+
+Adding character experience
+Improve our example RPG program to add an experience property named xp to the character. Its initial value is 0. Experience must appear in character description.
+
+// TODO: create the character object here
 let aurora = {
     name: 'Aurora',
     health: 150,
@@ -14,7 +44,9 @@ aurora.health -= 20;
 // Aurora equips a strength necklace
 aurora.strength += 10;
 
-// Aurora learnt a new skill
-aurora.xp += 15
+// Aurora learn a new skill
+aurora.xp += 15;
 
-console.log(aurora.describe())
+console.log(aurora.describe());
+
+Execution result
