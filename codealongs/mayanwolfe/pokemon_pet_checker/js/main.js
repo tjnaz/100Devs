32812409c4f7b7051c65119b9638bdc629046e85
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-29 15:38:37 +0530

    fix: url with '-' for images

--- codealongs/mayanwolfe/pokemon_pet_checker/js/main.js
+++ codealongs/mayanwolfe/pokemon_pet_checker/js/main.js
@@ -3,13 +3,21 @@ document.querySelector("button").addEventListener("click", getFetch);
 function getFetch() {
   const choice = document.querySelector("input").value;
   const url = `https://pokeapi.co/api/v2/pokemon/${choice}`;
+  // const img = `.official-artwork.front_default`;
 
   fetch(url)
     .then((res) => res.json()) // parse response as J Shaun
 
     .then((data) => {
-      console.log(data);
-      const potentialPet = new Poke();
+      console.log(data.sprites.other["official-artwork"].front_default);
+      console.log(data.sprites.other.dream_world.front_default);
+      const potentialPet = new Poke(
+        data.name,
+        data.height,
+        data.weight,
+        data.types,
+        data.sprites
+      );
     })
 
     .catch((err) => {
