13c4bcc16d9a466c8f432c3658ae43804c17721a
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-08 16:47:57 +0530

    finish: follow-through

--- practice/06-javascript-info/10_async/2_promise/main.js
+++ practice/06-javascript-info/10_async/2_promise/main.js
@@ -1,17 +1,19 @@
-let promise = new Promise(function (resolve, reject) {
-  // executor (the producing code)
+// // CODEALONG
+// let promise = new Promise(function (resolve, reject) {
+//   // executor (the producing code)
 
-  // setTimeout(() => resolve("done"), 1000);
-  setTimeout(() => reject(new Error("whoopsie daisy!!!")), 1000);
+//   setTimeout(() => resolve("done"), 1000);
+//   // setTimeout(() => reject(new Error("whoopsie daisy!!!")), 1000);
 
-  // resolve(console.log("done"));
-  // reject(new Error("..."));
-});
+//   // resolve(console.log("done"));
+//   // reject(new Error("..."));
+// });
 
-promise
-  .then(
-    (result) => console.log(`result: ${result}`) // 1st argument
-    // (error) => console.log(`error: ${error}`) // 2nd argument
-    // ); // something fishy happens here only when there's only one parameter for the 'then'
-  )
-  .catch((err) => console.log(`error: ${err}`));
+// promise
+//   .finally(() => console.log("stop right here"))
+//   .then(
+//     (result) => console.log(`result: ${result}`) // 1st argument
+//     // (error) => console.log(`error: ${error}`) // 2nd argument
+//     // ); // something fishy happens here only when there's only one parameter for the 'then'
+//   )
+//   .catch((err) => console.log(`error: ${err}`));
