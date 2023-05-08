481345ded36f0ebdba80eac286c3f7daf61f4b85
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-08 13:28:12 +0530

    feat: create a promise with executor that resolves and rejects

--- practice/06-javascript-info/10_async/2_promise/main.js
+++ practice/06-javascript-info/10_async/2_promise/main.js
@@ -1,3 +1,6 @@
 let promise = new Promise(function (resolve, reject) {
   // executor (the producing code)
+
+  setTimeout(() => resolve(console.log("done")), 1000);
+  setTimeout(() => reject(new Error(console.log("whoopsie daisy!!!"))), 1000);
 });
