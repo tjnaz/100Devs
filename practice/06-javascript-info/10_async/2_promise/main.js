a2f8f40e3d2bc4bfc1e3022ffed2913a9f07b2a6
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-08 13:36:31 +0530

    refactor: simplify the promise executor by removing timeouts

--- practice/06-javascript-info/10_async/2_promise/main.js
+++ practice/06-javascript-info/10_async/2_promise/main.js
@@ -1,6 +1,9 @@
 let promise = new Promise(function (resolve, reject) {
   // executor (the producing code)
 
-  setTimeout(() => resolve(console.log("done")), 1000);
-  setTimeout(() => reject(new Error(console.log("whoopsie daisy!!!"))), 1000);
+  // setTimeout(() => resolve(console.log("done")), 1000);
+  // setTimeout(() => reject(new Error(console.log("whoopsie daisy!!!"))), 1000);
+
+  resolve(console.log("done"));
+  // reject(new Error("..."));
 });
