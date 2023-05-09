1e3cc440e67112aa8bc20b06a27e80a9d1308e4e
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-09 12:43:18 +0530

    add: returning promises

--- practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
+++ practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
@@ -23,25 +23,62 @@
 //     return result * 2;
 //   });
 
-// not proper chaining
-let promise = new Promise(function (resolve, reject) {
+// // not proper chaining
+// let promise = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve(1);
+//   }, 1000);
+// });
+// promise.then(function (result) {
+//   console.log(result);
+//   return result * 2;
+// });
+// promise.then(function (result) {
+//   console.log(result);
+//   return result * 2;
+// });
+// promise.then(function (result) {
+//   console.log(result);
+//   return result * 2;
+// });
+// promise.then(function (result) {
+//   console.log(result);
+//   return result * 2;
+// });
+
+// Returning promises
+new Promise(function (resolve, reject) {
   setTimeout(() => {
     resolve(1);
   }, 1000);
-});
-promise.then(function (result) {
-  console.log(result);
-  return result * 2;
-});
-promise.then(function (result) {
-  console.log(result);
-  return result * 2;
-});
-promise.then(function (result) {
-  console.log(result);
-  return result * 2;
-});
-promise.then(function (result) {
-  console.log(result);
-  return result * 2;
-});
+})
+  .then(function (result) {
+    console.log(result);
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(result * 2);
+      }, 1000);
+    });
+  })
+  .then(function (result) {
+    console.log(result);
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(result * 2);
+      }, 1000);
+    });
+  })
+  .then(function (result) {
+    console.log(result);
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(result * 2);
+      }, 1000);
+    });
+  })
+  .then(function (result) {
+    console.log(result);
+  });
