9854a08c4005ac3760e4748f2dfe921136536a2e
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-09 11:46:33 +0530

    add: example of improper promises chaining

--- practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
+++ practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
@@ -1,24 +1,47 @@
 // Promise Chaining
 
-// 0. basic syntax
-new Promise(function (resolve, reject) {
+// // 0. basic syntax
+// new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve(1);
+//   }, 1000);
+// })
+//   .then(function (result) {
+//     console.log(result);
+//     return result * 2;
+//   })
+//   .then(function (result) {
+//     console.log(result);
+//     return result * 2;
+//   })
+//   .then(function (result) {
+//     console.log(result);
+//     return result * 2;
+//   })
+//   .then(function (result) {
+//     console.log(result);
+//     return result * 2;
+//   });
+
+// not proper chaining
+let promise = new Promise(function (resolve, reject) {
   setTimeout(() => {
     resolve(1);
   }, 1000);
-})
-  .then(function (result) {
-    console.log(result);
-    return result * 2;
-  })
-  .then(function (result) {
-    console.log(result);
-    return result * 2;
-  })
-  .then(function (result) {
-    console.log(result);
-    return result * 2;
-  })
-  .then(function (result) {
-    console.log(result);
-    return result * 2;
-  });
+});
+promise.then(function (result) {
+  console.log(result);
+  return result * 2;
+});
+promise.then(function (result) {
+  console.log(result);
+  return result * 2;
+});
+promise.then(function (result) {
+  console.log(result);
+  return result * 2;
+});
+promise.then(function (result) {
+  console.log(result);
+  return result * 2;
+});
