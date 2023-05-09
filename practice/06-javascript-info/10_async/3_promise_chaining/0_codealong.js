677fd1d1371a6e5d5e7a350fb6bca6c408c09588
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-09 14:16:10 +0530

    refactor: rewrite it with arrow function

--- practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
+++ practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
@@ -23,7 +23,26 @@
 //     return result * 2;
 //   });
 
-// // not proper chaining
+// refactor
+new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000))
+  .then((res) => {
+    console.log(res);
+    return res * 2;
+  })
+  .then((res) => {
+    console.log(res);
+    return res * 2;
+  })
+  .then((res) => {
+    console.log(res);
+    return res * 2;
+  })
+  .then((res) => {
+    console.log(res);
+    return res * 2;
+  });
+
+// // 1. not proper chaining
 // let promise = new Promise(function (resolve, reject) {
 //   setTimeout(() => {
 //     resolve(1);
@@ -46,39 +65,36 @@
 //   return result * 2;
 // });
 
-// Returning promises
-new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    resolve(1);
-  }, 1000);
-})
-  .then(function (result) {
-    console.log(result);
-
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(result * 2);
-      }, 1000);
-    });
-  })
-  .then(function (result) {
-    console.log(result);
-
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(result * 2);
-      }, 1000);
-    });
-  })
-  .then(function (result) {
-    console.log(result);
-
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(result * 2);
-      }, 1000);
-    });
-  })
-  .then(function (result) {
-    console.log(result);
-  });
+// // 2. Returning promises
+// new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve(1);
+//   }, 1000);
+// })
+//   .then(function (result) {
+//     console.log(result);
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(result * 2);
+//       }, 1000);
+//     });
+//   })
+//   .then(function (result) {
+//     console.log(result);
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(result * 2);
+//       }, 1000);
+//     });
+//   })
+//   .then(function (result) {
+//     console.log(result);
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(result * 2);
+//       }, 1000);
+//     });
+//   })
+//   .then(function (result) {
+//     console.log(result);
+//   });
