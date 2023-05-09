626a65a2a2905150663ad706d1cb8f90eb491172
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-09 11:45:38 +0530

    add: basic template for chaining

---
+++ practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
@@ -0,0 +1,24 @@
+// Promise Chaining
+
+// 0. basic syntax
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve(1);
+  }, 1000);
+})
+  .then(function (result) {
+    console.log(result);
+    return result * 2;
+  })
+  .then(function (result) {
+    console.log(result);
+    return result * 2;
+  })
+  .then(function (result) {
+    console.log(result);
+    return result * 2;
+  })
+  .then(function (result) {
+    console.log(result);
+    return result * 2;
+  });
