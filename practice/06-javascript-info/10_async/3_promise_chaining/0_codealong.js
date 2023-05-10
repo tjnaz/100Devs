1fc0d339bfc97fbda7a8a6c8fd891d02d33ee0ee
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-10 14:03:13 +0530

    add: 3. example loadScript()

--- practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
+++ practice/06-javascript-info/10_async/3_promise_chaining/0_codealong.js
@@ -99,29 +99,53 @@
 //     console.log(result);
 //   });
 
-// refactor: 2. Returning Promises
-new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000))
-  .then((result) => {
-    console.log(result);
-    return new Promise((resolve, reject) =>
-      setTimeout(() => resolve(result * 2), 1000)
-    );
-  })
-  .then((result) => {
-    console.log(result);
-    return new Promise((resolve, reject) =>
-      setTimeout(() => resolve(result * 2), 1000)
-    );
-  })
-  .then((result) => {
-    console.log(result);
-    return new Promise((resolve, reject) =>
-      setTimeout(() => resolve(result * 2), 1000)
-    );
-  })
-  .then((result) => {
-    console.log(result);
-    return new Promise((resolve, reject) =>
-      setTimeout(() => resolve(result * 2), 1000)
-    );
+// // refactor: 2. Returning Promises
+// new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000))
+//   .then((result) => {
+//     console.log(result);
+//     return new Promise((resolve, reject) =>
+//       setTimeout(() => resolve(result * 2), 1000)
+//     );
+//   })
+//   .then((result) => {
+//     console.log(result);
+//     return new Promise((resolve, reject) =>
+//       setTimeout(() => resolve(result * 2), 1000)
+//     );
+//   })
+//   .then((result) => {
+//     console.log(result);
+//     return new Promise((resolve, reject) =>
+//       setTimeout(() => resolve(result * 2), 1000)
+//     );
+//   })
+//   .then((result) => {
+//     console.log(result);
+//     return new Promise((resolve, reject) =>
+//       setTimeout(() => resolve(result * 2), 1000)
+//     );
+//   });
+
+// 3. example: loadScript
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement("script");
+    script.src = src;
+
+    script.onload = () => resolve(script);
+    script.onerror = () =>
+      reject(new Error(`!!!!Script load error for ${src}`));
+
+    document.body.append(script);
+  });
+}
+
+loadScript("./script.js")
+  .then((script) => loadScript("./one.js"))
+  .then((script) => loadScript("./two.js"))
+  .then((script) => loadScript("./three.js"))
+  .then((script) => {
+    one();
+    two();
+    three();
   });
---
+++ practice/06-javascript-info/10_async/3_promise_chaining/three.js
@@ -0,0 +1,3 @@
+function three() {
+  console.log("fn three");
+}
