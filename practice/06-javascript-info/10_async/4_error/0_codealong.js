138c7d6f002e5e6e68dd06c27b7ab799506ede9e
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-14 02:51:00 +0530

    add: two examples for catching error

--- practice/06-javascript-info/10_async/4_error/0_codealong.js
+++ practice/06-javascript-info/10_async/4_error/0_codealong.js
@@ -1,3 +1,23 @@
+// Error handling with promises
 fetch("https://no-such-server.blable")
   .then((res) => res.json())
   .catch((err) => console.log(err));
+
+fetch("https://javascript.info/article/promise-chaining/user.json")
+  .then((response) => response.json())
+  .then((user) => fetch(`https://api.github.com/users/${user.name}`))
+  .then((response) => response.json())
+  .then(
+    (githubUser) =>
+      new Promise((resolve, reject) => {
+        let img = document.createElement("img");
+        img.src = githubUser.avatar_url;
+        img.className = "promise-avatar-example";
+        document.body.append(img);
+        setTimeout(() => {
+          img.remove();
+          resolve(githubUser);
+        }, 3000);
+      })
+  )
+  .catch((error) => console.log(error.message));
