87ef4d9d5a274f6da22d409e7997df22dbcfa42b
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-16 06:17:00 +0530

    add: unhandled rejections

--- practice/06-javascript-info/10_async/4_error/0_codealong.js
+++ practice/06-javascript-info/10_async/4_error/0_codealong.js
@@ -31,30 +31,41 @@
 //   reject(new Error("whoopsie daisyy!!!"));
 // }).catch(console.log);
 
-// 2. Rethrowing
-// the execution: catch -> then
+// // 2. Rethrowing
+// // the execution: catch -> then
+// // new Promise((resolve, reject) => {
+// //   throw new Error("you done messed up aa ron");
+// // })
+// //   .catch((error) => {
+// //     console.log("the error is handled");
+// //   })
+// //   .then(() => console.log("next successful handler runs"));
+// // the execution: catch -> catch
 // new Promise((resolve, reject) => {
-//   throw new Error("you done messed up aa ron");
+//   throw new Error("every frkn time");
 // })
 //   .catch((error) => {
-//     console.log("the error is handled");
+//     if (error instanceof URIError) {
+//       // handle the error
+//     } else {
+//       console.log("can't handle the error, passing on...");
+//       throw error;
+//     }
 //   })
-//   .then(() => console.log("next successful handler runs"));
-// the execution: catch -> catch
+//   .then(() => {
+//     // doesn't run here
+//   })
+//   .catch((error) => {
+//     console.log(`the unknown error has occurred, ${error}`);
+//   });
+
+// 3. Undhandled rejections
+window.addEventListener("unhandledrejection", (event) => {
+  // the event object has TWO properties
+  console.log(event.promise); // [object Promise] - the promise that generated the error
+  console.log(event.reasnon); // Error: ${error} - the unhandled error object
+});
+
 new Promise((resolve, reject) => {
-  throw new Error("every frkn time");
-})
-  .catch((error) => {
-    if (error instanceof URIError) {
-      // handle the error
-    } else {
-      console.log("can't handle the error, passing on...");
-      throw error;
-    }
-  })
-  .then(() => {
-    // nothing runs here
-  })
-  .catch((error) => {
-    console.log(`the unknown error has occurred, ${error}`);
-  });
+  throw new Error("yo yo yo!");
+});
