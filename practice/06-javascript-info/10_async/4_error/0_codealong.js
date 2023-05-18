337456f8a1e030601394ea3e5f940833ebf254c7
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-18 16:47:32 +0530

    Revert "add: rethrowing"
    This reverts commit 23993128db20847957d6c1e92d3737bba9050f2f.

--- practice/06-javascript-info/10_async/4_error/0_codealong.js
+++ practice/06-javascript-info/10_async/4_error/0_codealong.js
@@ -1,5 +1,5 @@
 // Error handling with promises
-// NOTE// 0_Boilerplate
+// // 0_Boilerplate
 // fetch("https://no-such-server.blable")
 //   .then((res) => res.json())
 //   .catch((err) => console.log(err));
@@ -23,46 +23,11 @@
 //   )
 //   .catch((error) => console.log(error.message));
 
-// NOTE// 1. Implicit try...catch
-// new Promise((resolve, reject) => {
-//   throw new Error("Whoopsie daisie");
-// }).catch(console.log);
-// //
-// new Promise((resolve, reject) => {
-//   reject(new Error("whoopsie daisyy!!!"));
-// }).catch(console.log);
-// //
-// new Promise((resolve, reject) => {
-//   resolve("ok");
-// })
-//   .then((result) => {
-//     tnhsnhnst();
-//   })
-//   .catch(console.log);
-
-// NOTE// 2. Rethrowing
-// new Promise((resolve, reject) => {
-//   throw new Error("whoops");
-// })
-//   // .catch(console.log)
-//   .catch((error) => {
-//     console.log("The error is handled, continue normally");
-//   })
-//   .then(() => console.log("Next successful handler runs"));
+// 1. Implicit try...catch
+new Promise((resolve, reject) => {
+  throw new Error("Whoopsie daisie");
+}).catch(console.log);
 
 new Promise((resolve, reject) => {
-  throw new Error("error encountered");
-})
-  .catch((error) => {
-    if (error instanceof URIError) {
-      // handle the error
-      console.log("error encountered and dealt with");
-    } else {
-      console.log("can't handle the error");
-      throw error;
-    }
-  })
-  .then(() => {})
-  .catch((error) => {
-    console.log(`an unknow error has occurred, ${error}`);
-  });
+  reject(new Error("whoopsie daisyy!!!"));
+}).catch(console.log);
