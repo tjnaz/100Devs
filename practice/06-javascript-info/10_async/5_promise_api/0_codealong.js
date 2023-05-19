b0f6fea81d189377c1c5f6b05e4ead3da97b3038
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-19 11:19:00 +0530

    add: promise all example

--- practice/06-javascript-info/10_async/5_promise_api/0_codealong.js
+++ practice/06-javascript-info/10_async/5_promise_api/0_codealong.js
@@ -1,1 +1,6 @@
-
+// Promise.all
+Promise.all([
+  new Promise((resolve) => setTimeout(() => resolve(1), 3000)),
+  new Promise((resolve) => setTimeout(() => resolve(2), 2000)),
+  new Promise((resolve) => setTimeout(() => resolve(3), 1000)),
+]).then(console.log);
