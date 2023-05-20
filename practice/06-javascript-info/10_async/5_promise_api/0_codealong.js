1565a69795883606d0a0e4a52ee86eade8f0a4bf
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-20 17:03:42 +0530

    add: an error in the promise.all

--- practice/06-javascript-info/10_async/5_promise_api/0_codealong.js
+++ practice/06-javascript-info/10_async/5_promise_api/0_codealong.js
@@ -21,20 +21,29 @@
 //   )
 // );
 
-//A bigger example with fetching user information for an array of GitHub users by their names (we could fetch an array of goods by their ids, the logic is identical):
-let names = ["iliakan", "remy", "jeresig", "tjnaz"];
-let requests = names.map((name) =>
-  fetch(`https://api.github.com/users/${name}`)
-);
-Promise.all(requests)
-  .then((responses) => {
-    // all responses are resolved successfully
-    for (let response of responses) {
-      console.log(`${response.url}: ${response.status}`); // shows 200 for every url
-    }
-    return responses;
-  })
-  // map array of responses into an array of responses.json() to read their content
-  .then((responses) => Promise.all(responses.map((r) => r.json())))
-  // all JSON answers are parsed: "users" is the array of them
-  .then((users) => users.forEach((user) => console.log(user.name)));
+// //A bigger example with fetching user information for an array of GitHub users by their names (we could fetch an array of goods by their ids, the logic is identical):
+// let names = ["iliakan", "remy", "jeresig", "tjnaz"];
+// let requests = names.map((name) =>
+//   fetch(`https://api.github.com/users/${name}`)
+// );
+// Promise.all(requests)
+//   .then((responses) => {
+//     // all responses are resolved successfully
+//     for (let response of responses) {
+//       console.log(`${response.url}: ${response.status}`); // shows 200 for every url
+//     }
+//     return responses;
+//   })
+//   // map array of responses into an array of responses.json() to read their content
+//   .then((responses) => Promise.all(responses.map((r) => r.json())))
+//   // all JSON answers are parsed: "users" is the array of them
+//   .then((users) => users.forEach((user) => console.log(user.name)));
+
+// what if an error occurs as below
+Promise.all([
+  new Promise((resolve, reject) => setTimeout(() => resolve(1), 3000)),
+  new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error("oopsie daisy!!")), 2000)
+  ),
+  new Promise((resolve, reject) => setTimeout(() => resolve(3), 1000)),
+]).then(console.log);
