b2d2e111aaf88b89c7025084ff00ee2992459344
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-20 18:54:46 +0530

    add: promise.any

--- practice/06-javascript-info/10_async/5_promise_api/0_codealong.js
+++ practice/06-javascript-info/10_async/5_promise_api/0_codealong.js
@@ -65,12 +65,21 @@
 //   });
 // });
 
-// 3. Promise.race
-// Similar to Promise.all, but waits only for the first settled promise and gets its result (or error).
-Promise.race([
-  new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000)),
+// // 3. Promise.race
+// // Similar to Promise.all, but waits only for the first settled promise and gets its result (or error).
+// Promise.race([
+//   new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000)),
+//   new Promise((resolve, reject) =>
+//     setTimeout(() => reject(new Error("error")), 2000)
+//   ),
+//   new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000)),
+// ]).then(console.log);
+
+// 4. Promise.any
+Promise.any([
   new Promise((resolve, reject) =>
-    setTimeout(() => reject(new Error("error")), 2000)
+    setTimeout(() => reject(new Error("error")), 1000)
   ),
+  new Promise((resolve, reject) => setTimeout(() => resolve(1), 2000)),
   new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000)),
-]).then(console.log);
+]);
