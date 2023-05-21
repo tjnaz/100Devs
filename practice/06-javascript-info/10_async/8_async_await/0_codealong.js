ca76a95ce6b85a10a117d0b47e5b920c4221f6df
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-21 16:30:25 +0530

    add: showAvatar() example with multi. await + fetch

--- practice/06-javascript-info/10_async/8_async_await/0_codealong.js
+++ practice/06-javascript-info/10_async/8_async_await/0_codealong.js
@@ -6,16 +6,44 @@
 // //
 // f().then(console.log);
 
-// 2. await
-// The function execution "pauses" at the line (*) and resumes when the promise settles
-async function f() {
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("done"), 1000);
-  });
+// // 2. await
+// // The function execution "pauses" at the line (*) and resumes when the promise settles
+// async function f() {
+//   let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("done"), 1000);
+//   });
+//   //
+//   let result = await promise; // wait until the promise resolves(*)
+//   //
+//   console.log(result);
+// }
+// //
+// f();
+
+// 3. showAvatar()
+async function showAvatar() {
+  // read JSON
+  let response = await fetch(
+    `https://javascript.info/article/promise-chaining/user.json`
+  );
+  let user = await response.json();
   //
-  let result = await promise; // wait until the promise resolves(*)
+  // read github user
+  let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+  let githubUser = await githubResponse.json();
   //
-  console.log(result);
+  // show avatar
+  let img = document.createElement("img");
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+  //
+  // wait 3 seconds
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+  //
+  img.remove();
+  //
+  return githubUser;
 }
 //
-f();
+showAvatar();
