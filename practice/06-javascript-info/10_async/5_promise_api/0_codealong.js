21011def22b72c0056b09a23f4422732157f7351
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-21 13:02:23 +0530

    add: promise.any with no resolves; only errors

--- practice/06-javascript-info/10_async/5_promise_api/0_codealong.js
+++ practice/06-javascript-info/10_async/5_promise_api/0_codealong.js
@@ -75,11 +75,27 @@
 //   new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000)),
 // ]).then(console.log);
 
-// 4. Promise.any
-Promise.any([
-  new Promise((resolve, reject) =>
-    setTimeout(() => reject(new Error("error")), 1000)
-  ),
-  new Promise((resolve, reject) => setTimeout(() => resolve(1), 2000)),
-  new Promise((resolve, reject) => setTimeout(() => resolve(2), 3000)),
-]).then(console.log);
+// // 4. Promise.any
+// // Similar to Promise.race, but waits only for the first fulfilled promise and gets its result.
+// // If all of the given promises are rejected, then the returned promise is rejected
+// // with AggregateError – a special error object that stores all promise errors in its errors property.
+// Promise.any([
+//   new Promise((resolve, reject) =>
+//     setTimeout(() => reject(new Error("error")), 1000)
+//   ),
+//   new Promise((resolve, reject) => setTimeout(() => resolve(1), 2000)),
+//   new Promise((resolve, reject) => setTimeout(() => resolve(2), 3000)),
+// ]).then(console.log);
+// //
+// Promise.any([
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => reject(new Error("error 1")), 1000);
+//   }),
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => reject(new Error("error 2")), 2000);
+//   }),
+// ]).catch((error) => {
+//   console.log(error.constructor.name); // AggregateError
+//   console.log(error.errors[0]); // Error: Ouch!
+//   console.log(error.errors[1]); // Error: Error!
+// });
