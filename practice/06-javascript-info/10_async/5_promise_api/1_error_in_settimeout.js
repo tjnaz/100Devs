48f3a6bdf4f0c684eb4a0218e9a9a1f0c518b050
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-21 13:27:13 +0530

    refactor: remove unwanted file

--- practice/06-javascript-info/10_async/5_promise_api/1_error_in_settimeout.js
+++
@@ -1,7 +0,0 @@
-// What do you think? Will the .catch trigger? Explain your answer.
-
-new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    throw new Error("Whoops!");
-  }, 1000);
-}).catch(alert);
