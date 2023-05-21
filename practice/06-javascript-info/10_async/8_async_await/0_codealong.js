0e128e36366b1319e87106c71974f647aab75d63
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-21 16:16:16 +0530

    add: await syntax example

--- practice/06-javascript-info/10_async/8_async_await/0_codealong.js
+++ practice/06-javascript-info/10_async/8_async_await/0_codealong.js
@@ -1,7 +1,21 @@
 // 8_Async/Await
-// 1. async functions
+// // 1. async functions
+// async function f() {
+//   return 1;
+// }
+// //
+// f().then(console.log);
+
+// 2. await
+// The function execution "pauses" at the line (*) and resumes when the promise settles
 async function f() {
-  return 1;
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("done"), 1000);
+  });
+  //
+  let result = await promise; // wait until the promise resolves(*)
+  //
+  console.log(result);
 }
 //
-f().then(console.log);
+f();
