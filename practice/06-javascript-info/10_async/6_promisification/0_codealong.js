03ac8834c264882937d9fc42d229c7bbabae7583
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-21 15:07:26 +0530

    add: advance version of 'promisify'

--- practice/06-javascript-info/10_async/6_promisification/0_codealong.js
+++ practice/06-javascript-info/10_async/6_promisification/0_codealong.js
@@ -12,6 +12,7 @@ function loadScript(src, callback) {
   //
   document.head.append(script);
 }
+// // usage:
 // loadScript("./script.js", (error, script) => (error ? `Error` : newFunction()));
 
 // // promisify v1.0
@@ -23,33 +24,59 @@ function loadScript(src, callback) {
 //     });
 //   });
 // };
-// //
+// // usage:
 // loadScriptPromise("./script.js").then(() => newFunction());
 
-// promisify v2.0 -> helper
-// A call to 'promisify(f)' returns a wrapper around 'f(*)'. That wrapper returns a promise
-// and forwards the call to the original 'f', tracking the result in the custom callback(**)
-// **--** Here, promisify assumes that the original function expects a **--**
-// **--** callback with exactly two arguments (err, result). **--**
-function promisify(f) {
+// // promisify v2.0 -> helper
+// // A call to 'promisify(f)' returns a wrapper around 'f(*)'. That wrapper returns a promise
+// // and forwards the call to the original 'f', tracking the result in the custom callback(**)
+// // **--** Here, promisify assumes that the original function expects a **--**
+// // **--** callback with exactly two arguments (err, result). **--**
+// function promisify(f) {
+//   return function (...args) {
+//     // return a wrapper-function (*)
+//     return new Promise((resolve, reject) => {
+//       function callback(err, result) {
+//         // our custom callback for f(**)
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(result);
+//         }
+//       }
+//       //
+//       args.push(callback); // append our custom callback to the end of f arguments
+//       //
+//       f.call(this, ...args); // call the original function
+//     });
+//   };
+// }
+// // usage:
+// let loadScriptPromise = promisify(loadScript);
+// loadScriptPromise("./script.js").then(() => newFunction());
+
+// promisify v3.0 -> advance
+// But what if the original f expects a callback with more arguments callback(err, res1, res2, ...)?
+// 'promisify(f, true)' to get array of results
+function promisify(f, manyArgs = false) {
   return function (...args) {
-    // return a wrapper-function (*)
     return new Promise((resolve, reject) => {
-      function callback(err, result) {
-        // our custom callback for f(**)
+      function callback(err, ...results) {
+        // our custom callback for 'f'
         if (err) {
           reject(err);
         } else {
-          resolve(result);
+          // resolve with all callback results if manyArgs is spicified
+          resolve(manyArgs ? results : results[0]);
         }
       }
       //
-      args.push(callback); // append our custom callback to the end of f arguments
+      args.push(callback);
       //
-      f.call(this, ...args); // call the original function
+      f.call(this, ...args);
     });
   };
 }
-
-let loadScriptPromise = promisify(loadScript);
-loadScriptPromise("./script.js").then(() => newFunction());
+// usage:
+let f = promisify(loadScript, true);
+f("./script.js").then(() => newFunction());
