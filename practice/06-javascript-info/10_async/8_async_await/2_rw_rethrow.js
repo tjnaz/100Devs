b6c94a7bf33985a73848253fa6395d3fff7e1260
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-22 21:45:00 +0530

    finih: firt part of the task

--- practice/06-javascript-info/10_async/8_async_await/2_rw_rethrow.js
+++ practice/06-javascript-info/10_async/8_async_await/2_rw_rethrow.js
@@ -9,7 +9,6 @@
 //     this.response = response;
 //   }
 // }
-
 // function loadJson(url) {
 //   return fetch(url)
 //     .then(response => {
@@ -20,6 +19,49 @@
 //       }
 //     });
 // }
+// // Ask for a user name until github returns a valid user
+// function demoGithubUser() {
+//   let name = prompt("Enter a name?", "iliakan");
+//   return loadJson(`https://api.github.com/users/${name}`)
+//     .then(user => {
+//       alert(`Full name: ${user.name}.`);
+//       return user;
+//     })
+//     .catch(err => {
+//       if (err instanceof HttpError && err.response.status == 404) {
+//         alert("No such user, please reenter.");
+//         return demoGithubUser();
+//       } else {
+//         throw err;
+//       }
+//     });
+// }
+// demoGithubUser();
+
+// MY SOLUTION
+// test cases
+let name = "iliakan";
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = "HttpError";
+    this.response = response;
+  }
+}
+
+async function loadJson(url) {
+  let response = await fetch(url);
+
+  if (response.status == 200) {
+    // return response.json();
+    console.log(response.json());
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+loadJson(`https://api.github.com/users/${name}`);
 
 // // Ask for a user name until github returns a valid user
 // function demoGithubUser() {
