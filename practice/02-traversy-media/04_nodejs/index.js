0ac83332b89b24cbadb2e5e3a3ab5aee3499c4f3
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-27 12:24:27 +0530

    finish: import/export modules

--- practice/02-traversy-media/04_nodejs/index.js
+++ practice/02-traversy-media/04_nodejs/index.js
@@ -1,7 +1,9 @@
+// // 1. Basic import/export
 // // import Person from "./person"; // es6
-// const Person = require("./person"); // common js
-const person = require("./person"); // common js
+// const person = require("./person"); // common js
+// console.log(person.name);
 
-// const person1 = new Person("john doe", 30);
-
-console.log(person.name);
+// 2. using class
+const Person = require("./person"); // common js
+const person1 = new Person("john doe", 30);
+console.log(person1.greeting());
--- practice/02-traversy-media/04_nodejs/person.js
+++ practice/02-traversy-media/04_nodejs/person.js
@@ -1,6 +1,25 @@
-const person = {
-  name: "john doe",
-  age: 10,
-};
+// // Module Wrapper function
+// (function (exports, require, module, __filename, __dirname){
+// // ** function code ** //
+// })
+//
+// const person = {
+//   name: "john doe",
+//   age: 10,
+// };
+// module.exports = person;
 
-module.exports = person;
+console.log(__filename);
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greeting() {
+    return `the name's ${this.name}, ${this.age} old.`;
+  }
+}
+
+module.exports = Person;
