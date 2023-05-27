c284d727c1c199acb1a063fcdbe89f36cb768ef2
Thaj Anaz <anaz.thaj@gmail.com>
2023-05-27 16:19:23 +0530

    add: sample syntax

---
+++ practice/02-traversy-media/04_nodejs/reference/event_demo.js
@@ -0,0 +1,13 @@
+const EventEmitter = require("events");
+
+// Create class
+class MyEmitter extends EventEmitter {}
+
+// Initialise the object
+const myEmitter = new MyEmitter();
+
+// Event listener
+myEmitter.on("event", () => console.log("event fired"));
+
+// Initialise the event
+myEmitter.emit("event");
