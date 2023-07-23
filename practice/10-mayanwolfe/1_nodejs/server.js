5afda1e92310e996f8e988c6d55e28e200f84bcc
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-23 11:35:35 +0530

    add: demofile -> index.html

--- practice/10-mayanwolfe/1_nodejs/server.js
+++ practice/10-mayanwolfe/1_nodejs/server.js
@@ -6,7 +6,7 @@ const figlet = require("figlet");
 
 http
   .createServer((req, res) => {
-    fs.readFile("demofile.html", (err, data) => {
+    fs.readFile("index.html", (err, data) => {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       res.end();
