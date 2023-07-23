40a1dc6cae7da288494c6e89b294e0be0988ed27
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-23 10:48:01 +0530

    add: initial setting for testing the server

--- practice/10-mayanwolfe/1_nodejs/server.js
+++ practice/10-mayanwolfe/1_nodejs/server.js
@@ -3,3 +3,13 @@ const fs = require("fs");
 const url = require("url");
 const queryString = require("querystring");
 const figlet = require("figlet");
+
+http
+  .createServer((req, res) => {
+    fs.readFile("demofile.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  })
+  .listen(8000);
