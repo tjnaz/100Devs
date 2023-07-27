1a024fede4af2a06159314ed44057c7885b0d256
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-27 12:48:02 +0530

    add: import and connect the mongodb

--- practice/99_misc/2_zellwk/1-simple_crud_app/server.js
+++ practice/99_misc/2_zellwk/1-simple_crud_app/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const connectionString =
+  "mongodb+srv://yoda:ueoa@star-war-quotes.ewmevsi.mongodb.net/test?retryWrites=true&w=majority";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(
+  connectionString,
+  {
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) return console.log(err);
+    console.log("Connected to DB");
+  }
+);
 
 app.listen(3000, function () {
   console.log("listening on 3000");
