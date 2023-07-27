d8e9d8d691cd54646297fa493ef832a6c0ed7bdd
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-27 16:04:40 +0530

    add: mongodb server connection

--- practice/99_misc/2_zellwk/1-simple_crud_app/server.js
+++ practice/99_misc/2_zellwk/1-simple_crud_app/server.js
@@ -3,30 +3,34 @@ const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const connectionString =
-  "mongodb+srv://yoda:ueoa@star-war-quotes.ewmevsi.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://yoda:ueoa@star-war-quotes.ewmevsi.mongodb.net/star-war-quotes?retryWrites=true&w=majority";
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-MongoClient.connect(
-  connectionString,
-  {
-    useUnifiedTopology: true,
-  },
-  (err, client) => {
-    if (err) return console.log(err);
+MongoClient.connect(connectionString, {
+  useUnifiedTopology: true,
+})
+  .then((client) => {
+    const db = client.db("star-war-quotes");
+    const quotesCollection = db.collection("quotes");
     console.log("Connected to DB");
-  }
-);
 
-app.listen(3000, function () {
-  console.log("listening on 3000");
-});
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.listen(3000, function () {
+      console.log("listening on 3000");
+    });
 
-app.get("/", (req, res) => {
-  // res.send("Hello world");
-  res.sendFile(__dirname + "/index.html");
-});
+    app.get("/", (req, res) => {
+      // res.send("Hello world");
+      res.sendFile(__dirname + "/index.html");
+    });
 
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-});
+    app.post("/quotes", (req, res) => {
+      console.log(req.body);
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
+        })
+        .catch((error) => console.error(error));
+    });
+  })
+  .catch((err) => console.error(err));
