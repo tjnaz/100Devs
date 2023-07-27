5d072c4f852e7bf05db894e995c505391f045ec8
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-27 11:29:39 +0530

    add: import and use app.use body-parser

--- practice/99_misc/2_zellwk/1-simple_crud_app/server.js
+++ practice/99_misc/2_zellwk/1-simple_crud_app/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function () {
   console.log("listening on 3000");
