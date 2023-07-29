e9cb0cffa067573625000e24d4aa4926c239c0d4
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-29 12:55:28 +0530

    refactor: format code

--- classes/class-38-materials/client-side-code/js/main.js
+++ classes/class-38-materials/client-side-code/js/main.js
@@ -1,14 +1,16 @@
-document.querySelector('button').addEventListener('click', apiRequest)
+document.querySelector("button").addEventListener("click", apiRequest);
 
-async function apiRequest(){
-    const rapperName = document.querySelector('input').value
-    try{
-        const response = await fetch(`https://simple-rapper-api.herokuapp.com/api/${rapperName}`)
-        const data = await response.json()
+async function apiRequest() {
+  const rapperName = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://simple-rapper-api.herokuapp.com/api/${rapperName}`
+    );
+    const data = await response.json();
 
-        console.log(data)
-        document.querySelector('h2').innerText = data.birthName
-    }catch(error){
-        console.log(error)
-    }
+    console.log(data);
+    document.querySelector("h2").innerText = data.birthName;
+  } catch (error) {
+    console.log(error);
+  }
 }
