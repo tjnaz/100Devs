ee9401f454f7790e6b0494b6511caf27e7735640
Thaj Anaz <anaz.thaj@gmail.com>
2023-08-01 12:24:19 +0530

    add: redirect to / after the submission

--- practice/10-mayanwolfe/2_crud/codealong/server.js
+++ practice/10-mayanwolfe/2_crud/codealong/server.js
@@ -23,7 +23,10 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     app.post("/quotes", (req, res) => {
       quotesCollection
         .insertOne(req.body)
-        .then((result) => console.log(result))
+        .then((result) => {
+          console.log(result);
+          res.redirect("/");
+        })
         .catch((error) => console.error(err));
     });
 
