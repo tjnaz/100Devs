ce1a07d3d00ee3a4b55d39e24c9fc94576d1b5dd
Thaj Anaz <anaz.thaj@gmail.com>
2023-08-01 11:28:54 +0530

    add: rearrange the app code to be inside the mongodb connection

--- practice/10-mayanwolfe/2_crud/codealong/server.js
+++ practice/10-mayanwolfe/2_crud/codealong/server.js
@@ -3,26 +3,28 @@ const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const connectionString =
-  "mongodb+srv://yoda:ueoa@star-war-quotes.ewmevsi.mongodb.net/star-war-quotes?retryWrites=true&w=majority";
+  "mongodb+srv://vader:ueoa@star-wars-quotes.djydzot.mongodb.net/?retryWrites=true&w=majority";
 
 // const connectionString2 =
 //   "mongodb+srv://yoda:ueoa@star-war-quotes.ewmevsi.mongodb.net/?retryWrites=true&w=majority";
 
-MongoClient.connect(connectionString, (err, client) => {
-  if (err) return console.error(err);
-  console.log("Connected to DB");
-});
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to DB");
+    const db = client.db("star-wars-quotes");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
-  console.log("listening on 3000");
-});
+    app.listen(3000, function () {
+      console.log("listening on 3000");
+    });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
 
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-});
+    app.post("/quotes", (req, res) => {
+      console.log(req.body);
+    });
+  })
+  .catch((err) => console.error(err));
