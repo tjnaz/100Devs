122cbe915182a43db5e8cb1980b2e79975321a1c
Thaj Anaz <anaz.thaj@gmail.com>
2023-08-03 22:22:33 +0530

    add: make the public folder accessible

--- practice/10-mayanwolfe/2_crud/codealong/server.js
+++ practice/10-mayanwolfe/2_crud/codealong/server.js
@@ -18,6 +18,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 
     app.use(bodyParser.urlencoded({ extended: true }));
 
+    app.use(express.static("public"));
+
     app.get("/", (req, res) => {
       quotesCollection
         .find()
