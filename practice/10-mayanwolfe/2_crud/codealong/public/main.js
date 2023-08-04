a63c7b09c789fd065a2a14ceab5405dd5618e958
Thaj Anaz <anaz.thaj@gmail.com>
2023-08-04 10:48:08 +0530

    add: json app method with a sample quote

--- practice/10-mayanwolfe/2_crud/codealong/public/main.js
+++ practice/10-mayanwolfe/2_crud/codealong/public/main.js
@@ -3,5 +3,10 @@ const update = document.querySelector("#update-button");
 update.addEventListener("click", (_) => {
   fetch("/quotes", {
     method: "put",
+    header: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: "Darth Vader",
+      quote: "I find your lack of faith disturbing.",
+    }),
   });
 });
