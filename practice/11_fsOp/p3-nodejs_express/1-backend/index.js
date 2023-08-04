0dadf3b039e1a4574232a4543e3890f4feb40ea9
Thaj Anaz <anaz.thaj@gmail.com>
2023-08-04 16:31:18 +0530

    add: create a simple web server

---
+++ practice/11_fsOp/p3-nodejs_express/1-backend/index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello backend - p3: nodejs and express");
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
