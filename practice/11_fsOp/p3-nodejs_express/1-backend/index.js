ff3293660ecb7b298c942358f275f07a4548ed35
Thaj Anaz <anaz.thaj@gmail.com>
2023-08-04 16:41:30 +0530

    add: some json stringify data

--- practice/11_fsOp/p3-nodejs_express/1-backend/index.js
+++ practice/11_fsOp/p3-nodejs_express/1-backend/index.js
@@ -1,8 +1,27 @@
+// app imports node's built-in web server module
 const http = require("http");
 
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "browser can execute only js",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hello backend - p3: nodejs and express");
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(notes));
 });
 
 const PORT = 3001;
