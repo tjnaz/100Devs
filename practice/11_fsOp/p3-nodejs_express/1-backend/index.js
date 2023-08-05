8982dd2efc7c4f19b8cbbdbfad101fdef14b3ded
Thaj Anaz <anaz.thaj@gmail.com>
2023-08-05 13:37:12 +0530

    test: console.log tests to record the id & note param

--- practice/11_fsOp/p3-nodejs_express/1-backend/index.js
+++ practice/11_fsOp/p3-nodejs_express/1-backend/index.js
@@ -32,7 +32,9 @@ app.get("/", (req, res) => {
 
 app.get("/api/notes/:id", (req, res) => {
   const id = req.params.id;
+  console.log(id);
   const note = notes.find((note) => note.id === id);
+  console.log(note);
   res.json(note);
 });
 
